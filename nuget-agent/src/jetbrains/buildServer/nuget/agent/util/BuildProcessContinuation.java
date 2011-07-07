ca98464f8f99be9529c1271fb2172909a6b8af64
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-07-07 17:56:51 +0400

    extract BuildProcessBase, introduce more interfaces for CompositeBuildProcess

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/util/BuildProcessContinuation.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/util/BuildProcessContinuation.java
@@ -17,11 +17,12 @@
 package jetbrains.buildServer.nuget.agent.util;
 
 import jetbrains.buildServer.agent.BuildProcess;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
- * Date: 07.07.11 15:37
+ * Date: 07.07.11 16:09
  */
-public interface CompositeBuildProcess extends BuildProcess, BuildProcessContinuation {
-
+public interface BuildProcessContinuation {
+  void pushBuildProcess(@NotNull BuildProcess process);
 }
--- nuget-agent/src/jetbrains/buildServer/nuget/agent/util/impl/CompositeBuildProcessImpl.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/util/impl/CompositeBuildProcessImpl.java
@@ -19,63 +19,52 @@ package jetbrains.buildServer.nuget.agent.util.impl;
 import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.BuildFinishedStatus;
 import jetbrains.buildServer.agent.BuildProcess;
+import jetbrains.buildServer.nuget.agent.util.BuildProcessBase;
 import jetbrains.buildServer.nuget.agent.util.CompositeBuildProcess;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
  * Date: 07.07.11 14:04
  */
-public class CompositeBuildProcessImpl implements CompositeBuildProcess {
+public class CompositeBuildProcessImpl extends BuildProcessBase implements CompositeBuildProcess {
   private final BlockingQueue<BuildProcess> myProcessList = new LinkedBlockingQueue<BuildProcess>();
-  private final AtomicBoolean myIsInterrupted = new AtomicBoolean();
-  private final AtomicBoolean myIsFinished = new AtomicBoolean();
   private final AtomicReference<BuildProcess> myCurrentProcess = new AtomicReference<BuildProcess>();
 
   public void pushBuildProcess(@NotNull final BuildProcess process) {
     myProcessList.add(process);
   }
 
-  public void start() throws RunBuildException {
-  }
-
-  public boolean isInterrupted() {
-    return myIsInterrupted.get();
-  }
-
-  public boolean isFinished() {
-    return myIsFinished.get();
-  }
-
-  public void interrupt() {
-    myIsInterrupted.set(true);
+  @Override
+  protected void interruptImpl() {
     BuildProcess process = myCurrentProcess.get();
     if (process != null) {
       process.interrupt();
     }
   }
 
+  public void start() throws RunBuildException {
+  }
+
   @NotNull
-  public BuildFinishedStatus waitFor() throws RunBuildException {
-    for(BuildProcess proc = myProcessList.poll(); proc != null; proc = myProcessList.poll()) {
+  protected BuildFinishedStatus waitForImpl() throws RunBuildException {
+    if (isInterrupted()) return BuildFinishedStatus.INTERRUPTED;
+    for (BuildProcess proc = myProcessList.poll(); proc != null; proc = myProcessList.poll()) {
       myCurrentProcess.set(proc);
       try {
         proc.start();
         BuildFinishedStatus status = proc.waitFor();
-        if (status == BuildFinishedStatus.FINISHED_FAILED ) return BuildFinishedStatus.FINISHED_FAILED;
+        if (status == BuildFinishedStatus.FINISHED_FAILED) return BuildFinishedStatus.FINISHED_FAILED;
       } finally {
         myCurrentProcess.set(null);
       }
       if (isInterrupted()) return BuildFinishedStatus.INTERRUPTED;
     }
-    myIsFinished.set(true);
     if (isInterrupted()) return BuildFinishedStatus.INTERRUPTED;
-
     return BuildFinishedStatus.FINISHED_SUCCESS;
   }
 }
