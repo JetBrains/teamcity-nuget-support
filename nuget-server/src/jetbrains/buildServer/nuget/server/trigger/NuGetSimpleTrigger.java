cc158723b55296153ae82c9d09a7cc0beed41095
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-07-12 15:58:44 +0400

    support validation

--- nuget-server/src/jetbrains/buildServer/nuget/server/trigger/NuGetSimpleTrigger.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/trigger/NuGetSimpleTrigger.java
@@ -1,8 +1,15 @@
 package jetbrains.buildServer.nuget.server.trigger;
 
 import jetbrains.buildServer.buildTriggers.*;
+import jetbrains.buildServer.serverSide.InvalidProperty;
+import jetbrains.buildServer.serverSide.PropertiesProcessor;
+import jetbrains.buildServer.util.StringUtil;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -39,6 +46,30 @@ public class NuGetSimpleTrigger extends BuildTriggerService {
     return new PolledBuildTrigger() {
       @Override
       public void triggerBuild(@NotNull final PolledTriggerContext context) throws BuildTriggerException {
+      }
+    };
+  }
+
+  @Override
+  public boolean isMultipleTriggersPerBuildTypeAllowed() {
+    return true;
+  }
+
+  @Override
+  public PropertiesProcessor getTriggerPropertiesProcessor() {
+    return new PropertiesProcessor() {
+      public Collection<InvalidProperty> process(Map<String, String> properties) {
+        Collection<InvalidProperty> err = new ArrayList<InvalidProperty>();
+
+        if (StringUtil.isEmptyOrSpaces(properties.get(TriggerConstants.SOURCE))) {
+          err.add(new InvalidProperty(TriggerConstants.SOURCE, "Source must be specified"));
+        }
+
+        if (StringUtil.isEmptyOrSpaces(properties.get(TriggerConstants.PACKAGE))) {
+          err.add(new InvalidProperty(TriggerConstants.PACKAGE, "Package Id must be specified"));
+        }
+
+        return err;
       }
     };
   }
