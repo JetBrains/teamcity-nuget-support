e26ec4101f7b4bc964a2d82319c9eaae18777983
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-07-13 18:19:45 +0400

    add NuGet smaple extension code, create NuGet runner wrapper

---
+++ nuget-extensions/nuget-commands/src/NuGetTeamCityListCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NuGet;
+using NuGet.Commands;
+
+namespace JetBrains.TeamCity.NuGet.ExtendedCommands
+{
+  [Command("TeamCity.List", "Lists packages for given Id with parsable output")]
+  public class NuGetTeamCityListCommand : Command
+  {
+    private readonly List<string> mySources = new List<string>();
+
+    [Option("Sources to search for package", AltName = "s")]
+    public ICollection<string> Source { get { return mySources; } }
+
+    [Option("Package Id to check for version update")]
+    public string Id { get; set; }
+
+    [Option("NuGet Version Spec to constraint versions to be checked. Optional")]
+    public string Version { get; set; }
+
+    public override void ExecuteCommand()
+    {
+      System.Console.Out.WriteLine("TeamCity NuGet List command.");
+      System.Console.Out.WriteLine("Sources: {0}", string.Join(", ", mySources.ToArray()));
+      System.Console.Out.WriteLine("Package Id: {0}", Id ?? "<null>");
+      System.Console.Out.WriteLine("Version: {0}", Version ?? "<null>");
+    }
+  }
+}
---
+++ nuget-extensions/nuget-runner/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace nuget_runner
+{
+  public class Program
+  {
+    static int Main(string[] args)
+    {
+      Console.Out.WriteLine("JetBrains TeamCity NuGet Runner " + typeof(Program).Assembly.GetName().Version);
+      Console.Out.WriteLine("Starting NuGet with additional commands");
+      if (args.Length < 2) return Usage();
+
+      string nuget = args[0];
+      string[] nugetArgs = args.Skip(1).ToArray();
+
+      var nugetAssembly = Assembly.LoadFrom(nuget);
+
+      var result = nugetAssembly.EntryPoint.Invoke(null, new[] {nugetArgs});
+
+      if (result is int)
+        return (int) result;
+
+      return 0;
+    }
+
+    static int Usage()
+    {
+      Console.Out.WriteLine("JetBrains.TeamCity.NuGetRunner.exe <path to nuget> <nuget parameters>");
+      return 1;
+    }
+  }
+}
