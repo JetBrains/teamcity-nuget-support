8a5e3a59ab9d0f0a04cb0723ab07d9f3a4dfb596
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-08-10 20:56:18 +0400

    server settings nuget tab + security + mock content

---
+++ nuget-server/src/jetbrains/buildServer/nuget/server/toolRegistry/tab/ServerSettingsController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.nuget.server.toolRegistry.tab;
+
+import jetbrains.buildServer.controllers.AuthorizationInterceptor;
+import jetbrains.buildServer.controllers.BaseController;
+import jetbrains.buildServer.controllers.RequestPermissionsChecker;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.auth.AccessDeniedException;
+import jetbrains.buildServer.serverSide.auth.AuthorityHolder;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.WebControllerManager;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
+ * Date: 10.08.11 20:38
+ */
+public class ServerSettingsController extends BaseController {
+  private final String myPath;
+
+  public ServerSettingsController(@NotNull final SBuildServer server,
+                                  @NotNull final AuthorizationInterceptor auth,
+                                  @NotNull final PermissionChecker checker,
+                                  @NotNull final WebControllerManager web,
+                                  @NotNull final PluginDescriptor descriptor) {
+    super(server);
+    myPath = descriptor.getPluginResourcesPath("tool/nuget-server-tab.html");
+    auth.addPathBasedPermissionsChecker(myPath, new RequestPermissionsChecker() {
+      public void checkPermissions(@NotNull AuthorityHolder authorityHolder, @NotNull HttpServletRequest request) throws AccessDeniedException {
+       checker.assertAccess(authorityHolder);
+      }
+    });
+    web.registerController(myPath, this);
+  }
+
+  @NotNull
+  public String getPath() {
+    return myPath;
+  }
+
+  @Override
+  protected ModelAndView doHandle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    return simpleView("Empty nuget tab content");
+  }
+}
---
+++ nuget-server/src/jetbrains/buildServer/nuget/server/toolRegistry/tab/ServerSettingsTab.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.nuget.server.toolRegistry.tab;
+
+import jetbrains.buildServer.web.openapi.PagePlaces;
+import jetbrains.buildServer.web.openapi.PlaceId;
+import jetbrains.buildServer.web.openapi.PositionConstraint;
+import jetbrains.buildServer.web.openapi.SimpleCustomTab;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
+ * Date: 10.08.11 20:35
+ */
+public class ServerSettingsTab extends SimpleCustomTab {
+  private final PermissionChecker myChecker;
+
+  public ServerSettingsTab(@NotNull final PagePlaces pagePlaces,
+                           @NotNull final ServerSettingsController controller,
+                           @NotNull final PermissionChecker checker) {
+    super(pagePlaces,
+            PlaceId.ADMIN_SERVER_CONFIGURATION_TAB,
+            "toolLoadTab",
+            controller.getPath(),
+            "NuGet");
+    myChecker = checker;
+    setPosition(PositionConstraint.last());
+    register();
+  }
+
+  @Override
+  public boolean isVisible() {
+    return super.isVisible() && myChecker.hasAccess();
+  }
+}
