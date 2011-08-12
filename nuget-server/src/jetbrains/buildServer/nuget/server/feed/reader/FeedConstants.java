5487b833f75a28278cd1aa75edabf7dfc8732793
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-08-12 15:12:27 +0400

    move constants interface

--- nuget-server/src/jetbrains/buildServer/nuget/server/feed/reader/FeedConstants.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/reader/FeedConstants.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package jetbrains.buildServer.nuget.server.feed.reader.impl;
+package jetbrains.buildServer.nuget.server.feed.reader;
 
 /**
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
