4ba7cfc505a11a10c790b80f7c9df1a997deed00
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-10-07 20:28:14 +0200

    add index build method and service, add test

---
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/NuGetServerRegister.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.nuget.server.feed.server;
+
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.nuget.server.ToolPaths;
+import jetbrains.buildServer.nuget.server.feed.FeedClient;
+import jetbrains.buildServer.nuget.server.feed.reader.impl.FeedGetMethodFactory;
+import jetbrains.buildServer.nuget.server.feed.reader.impl.Param;
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.SecurityContextEx;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
+import jetbrains.buildServer.util.ExceptionUtil;
+import jetbrains.buildServer.util.FileUtil;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ *         Date: 07.10.11 19:42
+ */
+public class NuGetServerRegister {
+  private static final Logger LOG = Logger.getInstance(NuGetServerRegister.class.getName());
+
+  private final FeedClient myClient;
+  private final NuGetServerUri myUris;
+  private final ToolPaths myPaths;
+  private final SecurityContextEx mySecurityContext;
+  private final FeedGetMethodFactory myGetMethodFactory;
+
+  public NuGetServerRegister(@NotNull final FeedClient client,
+                             @NotNull final NuGetServerUri uris,
+                             @NotNull final ToolPaths paths,
+                             @NotNull final SecurityContextEx securityContextEx,
+                             @NotNull final FeedGetMethodFactory getMethodFactory) {
+    myClient = client;
+    myUris = uris;
+    myPaths = paths;
+    mySecurityContext = securityContextEx;
+    myGetMethodFactory = getMethodFactory;
+  }
+
+  public void registerPackage(@NotNull final SBuild build, @NotNull final String path) throws NuGetFeedException {
+    try {
+      mySecurityContext.runAsSystem(new SecurityContextEx.RunAsAction() {
+        public void run() throws Throwable {
+          registerPackageImpl(build, path);
+        }
+      });
+    } catch (NuGetFeedException e) {
+      throw e;
+    } catch (Throwable t) {
+      ExceptionUtil.rethrowAsRuntimeException(t);
+    }
+  }
+
+
+  private void registerPackageImpl(@NotNull final SBuild build, @NotNull final String path) throws NuGetFeedException {
+    final BuildArtifact artifact = build.getArtifacts(BuildArtifactsViewMode.VIEW_ALL).getArtifact(path);
+    if (artifact == null) {
+      LOG.warn("Failed to find artifact " + path + " of build buildId=" + build.getBuildId());
+      return;
+    }
+
+    final String buildType = build.getBuildTypeId();
+    final String buildId = String.valueOf(build.getBuildId());
+    final String downloadUrl = "/repository/download/" + build.getBuildTypeId() + "/" + build.getBuildId() + ":id/" + path;
+    final String filePath = FileUtil.getRelativePath(myPaths.getArtifactsDirectory(), new File(build.getArtifactsDirectory(), artifact.getRelativePath()));
+
+    if (filePath == null) {
+      throw new NuGetFeedException("Failed to map artifact path") {{ setRecoverable(false);}};
+    }
+
+    final String addPackageUri = myUris.getAddPackageUri();
+    if (addPackageUri == null) {
+      throw new NuGetFeedException("Failed to connect to NuGet Feed server. Server is not running");
+    }
+
+    final HttpGet get = myGetMethodFactory.createGet(
+            addPackageUri,
+            new Param("buildType", buildType),
+            new Param("buildId", buildId),
+            new Param("downloadUrl", downloadUrl),
+            new Param("packageFile", filePath)
+    );
+
+    final HttpResponse response;
+    try {
+      response = myClient.execute(get);
+    } catch (IOException e) {
+      throw new NuGetFeedException("Failed to execute request. " + e.getMessage(), e);
+    }
+
+    if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+      throw new NuGetFeedException("Request was not acceped by server: " + response.getStatusLine().getReasonPhrase());
+    }
+  }
+}
---
+++ nuget-tests/src/jetbrains/buildServer/nuget/tests/server/feed/server/NuGetServerRegisterTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.nuget.tests.server.feed.server;
+
+import jetbrains.buildServer.BaseTestCase;
+import jetbrains.buildServer.nuget.server.ToolPaths;
+import jetbrains.buildServer.nuget.server.feed.FeedClient;
+import jetbrains.buildServer.nuget.server.feed.reader.impl.FeedGetMethodFactory;
+import jetbrains.buildServer.nuget.server.feed.server.NuGetFeedException;
+import jetbrains.buildServer.nuget.server.feed.server.NuGetServerRegister;
+import jetbrains.buildServer.nuget.server.feed.server.NuGetServerUri;
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.SecurityContextEx;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifacts;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
+import jetbrains.buildServer.util.ExceptionUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.RequestLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.api.Invocation;
+import org.jmock.lib.action.CustomAction;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ *         Date: 07.10.11 20:08
+ */
+public class NuGetServerRegisterTest extends BaseTestCase {
+  private Mockery m;
+  private FeedClient myClient;
+  private NuGetServerUri myUris;
+  private ToolPaths myPaths;
+  private SecurityContextEx mySecurityContext;
+  private SBuild myBuild;
+  private BuildArtifacts myBuildArtifacts;
+  private BuildArtifact myArtifacts;
+  private NuGetServerRegister myReg;
+
+
+  @BeforeMethod
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    m = new Mockery();
+    myClient = m.mock(FeedClient.class);
+    myUris = m.mock(NuGetServerUri.class);
+    myPaths = m.mock(ToolPaths.class);
+    mySecurityContext = m.mock(SecurityContextEx.class);
+    myBuild = m.mock(SBuild.class);
+    myBuildArtifacts = m.mock(BuildArtifacts.class);
+    myArtifacts = m.mock(BuildArtifact.class);
+    myReg = new NuGetServerRegister(myClient, myUris, myPaths, mySecurityContext, new FeedGetMethodFactory());
+
+    final File temp = createTempDir();
+    m.checking(new Expectations(){{
+      try {
+        allowing(mySecurityContext).runAsSystem(with(any(SecurityContextEx.RunAsAction.class)));
+      } catch (Throwable throwable) {
+        ExceptionUtil.rethrowAsRuntimeException(throwable);
+      }
+      will(new CustomAction("Execute") {
+        public Object invoke(Invocation invocation) throws Throwable {
+          ((SecurityContextEx.RunAsAction)invocation.getParameter(0)).run();
+          return null;
+        }
+      });
+      allowing(myBuild).getArtifacts(BuildArtifactsViewMode.VIEW_ALL); will(returnValue(myBuildArtifacts));
+      allowing(myBuild).getBuildId(); will(returnValue(555L));
+      allowing(myBuild).getBuildTypeId(); will(returnValue("btX"));
+
+      allowing(myPaths).getArtifactsDirectory(); will(returnValue(temp));
+      allowing(myBuild).getArtifactsDirectory(); will(returnValue(new File(temp, "a/b/c")));
+
+    }});
+  }
+
+  @Test
+  public void testCallRemote() throws NuGetFeedException, IOException {
+    final String path = "aaa/super.1.2.3.4.nupkg";
+
+    final HttpResponse response = m.mock(HttpResponse.class);
+    final org.apache.http.StatusLine statusLine = m.mock(org.apache.http.StatusLine.class);
+
+    m.checking(new Expectations(){{
+      allowing(myBuildArtifacts).getArtifact(path); will(returnValue(myArtifacts));
+      allowing(myArtifacts).getRelativePath(); will(returnValue(path));
+
+      allowing(myUris).getAddPackageUri(); will(returnValue("http://localhost:9999/nuget4/add"));
+
+      oneOf(myClient).execute(with(new BaseMatcher<HttpUriRequest>(){
+        public boolean matches(Object o) {
+          HttpGet get = (HttpGet) o;
+          final RequestLine requestLine = get.getRequestLine();
+          final String uri = requestLine.getUri();
+          System.out.println("uri = " + uri);
+          return "http://localhost:9999/nuget4/add?buildType=btX&buildId=555&downloadUrl=%2Frepository%2Fdownload%2FbtX%2F555%3Aid%2Faaa%2Fsuper.1.2.3.4.nupkg&packageFile=a%5Cb%5Cc%5Caaa%5Csuper.1.2.3.4.nupkg".equals(uri);
+        }
+
+        public void describeTo(Description description) {
+
+        }
+      }));
+      will(returnValue(response));
+
+      allowing(response).getStatusLine(); will(returnValue( statusLine));
+      allowing(statusLine).getStatusCode(); will(returnValue(200));
+    }});
+
+
+    myReg.registerPackage(myBuild, path);
+  }
+
+
+
+}
