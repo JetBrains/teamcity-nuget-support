2aacc91b8d773b26bcbc40704022902dcedd7a1f
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-10-07 16:16:18 +0200

    ping server to check it alive

--- nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/NuGetServerCruiser.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/NuGetServerCruiser.java
@@ -18,28 +18,41 @@ package jetbrains.buildServer.nuget.server.feed.server;
 
 import jetbrains.buildServer.serverSide.BuildServerAdapter;
 import jetbrains.buildServer.serverSide.BuildServerListener;
+import jetbrains.buildServer.serverSide.executors.ExecutorServices;
 import jetbrains.buildServer.util.EventDispatcher;
+import jetbrains.buildServer.util.ExceptionUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
  *         Date: 07.10.11 15:27
  */
 public class NuGetServerCruiser {
-  private final NuGetServerRunner myRunner;
-
   public NuGetServerCruiser(@NotNull final NuGetServerRunner runner,
+                            @NotNull final ExecutorServices executors,
                             @NotNull final EventDispatcher<BuildServerListener> events) {
-    myRunner = runner;
-
     events.addListener(new BuildServerAdapter(){
+      private ScheduledFuture<?> myCheckTask;
       @Override
       public void serverStartup() {
         runner.startServer();
+        myCheckTask = executors.getNormalExecutorService().scheduleWithFixedDelay(ExceptionUtil.catchAll("Check NuGet Server is running task", new Runnable() {
+          public void run() {
+            runner.ensureAlive();
+          }
+        }), 10, 10, TimeUnit.SECONDS);
       }
 
       @Override
       public void serverShutdown() {
+        final Future<?> task = myCheckTask;
+        myCheckTask = null;
+        task.cancel(false);
+
         runner.stopServer();
       }
     });
