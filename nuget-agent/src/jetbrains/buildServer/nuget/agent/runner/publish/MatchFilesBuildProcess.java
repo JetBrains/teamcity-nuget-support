ffe36d42d7b63b8e9e84aab95289a70437fbbd9e
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-10-09 23:27:30 +0200

    extract base class for file matchers

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/publish/MatchFilesBuildProcess.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/publish/MatchFilesBuildProcess.java
@@ -21,9 +21,8 @@ import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.BuildFinishedStatus;
 import jetbrains.buildServer.agent.BuildRunnerContext;
 import jetbrains.buildServer.nuget.agent.parameters.NuGetPublishParameters;
+import jetbrains.buildServer.nuget.agent.util.MatchFilesBuildProcessBase;
 import jetbrains.buildServer.nuget.agent.util.fsScanner.DirectoryScanner;
-import jetbrains.buildServer.nuget.agent.util.fsScanner.DirectoryScanner;
-import jetbrains.buildServer.nuget.agent.util.BuildProcessBase;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,57 +36,26 @@ import java.util.List;
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
  * Date: 21.07.11 20:02
  */
-public class MatchFilesBuildProcess extends BuildProcessBase {
-  private static final Logger LOG = Logger.getInstance(MatchFilesBuildProcess.class.getName());
-
-  private final BuildRunnerContext myContext;
+public class MatchFilesBuildProcess extends MatchFilesBuildProcessBase {
   private final NuGetPublishParameters myParameters;
-  private final Callback myCallback;
 
   public MatchFilesBuildProcess(@NotNull final BuildRunnerContext context,
                                 @NotNull final NuGetPublishParameters parameters,
                                 @NotNull final Callback callback) {
-    myContext = context;
+    super(context, callback);
     myParameters = parameters;
-    myCallback = callback;
   }
 
   @NotNull
   @Override
-  protected BuildFinishedStatus waitForImpl() throws RunBuildException {
-
-    boolean found = false;
-
-    final List<String> patterns = new ArrayList<String>();
-    for (String _pattern : myParameters.getFiles()) {
-      String pattern = _pattern.trim();
-      if (StringUtil.isEmptyOrSpaces(pattern)) {
-        continue;
-      }
-
-      patterns.add(pattern);
-    }
-
-    final File root = myContext.getBuild().getCheckoutDirectory();
-    try {
-      final Collection<File> result = DirectoryScanner.findFiles(root, patterns, Collections.<String>emptyList());
-      for (File file : result) {
-        LOG.debug("Found nugkg to push: " + file);
-        found = true;
-        myCallback.fileFound(file);
-      }
-    } catch (Exception e) {
-      throw new RunBuildException("Failed to find packages to publish. " + e.getMessage(), e);
-    }
-
-    if (!found) {
-      throw new RunBuildException("Failed to find files to publish matching: " + new ArrayList<String>(myParameters.getFiles()) + " under " + root + ". No packages to publish. ");
-    }
-
-    return BuildFinishedStatus.FINISHED_SUCCESS;
+  protected String getActionName() {
+    return "create packages";
   }
 
-  public static interface Callback {
-    void fileFound(@NotNull final File file) throws RunBuildException;
+  @NotNull
+  @Override
+  protected Collection<String> getFiles() throws RunBuildException {
+    return myParameters.getFiles();
+
   }
 }
