2bab354b1d5eecc025fbbf54835562591bf154f7
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-10-11 19:55:03 +0200

    introduce tree normalization

--- nuget-extensions/nuget-feed/Query/Tree/FilterTreeNode.cs
+++ nuget-extensions/nuget-feed/Query/Tree/FilterTreeNode.cs
@@ -2,5 +2,9 @@ namespace JetBrains.TeamCity.NuGet.Feed.Query.Tree
 {
   public abstract class FilterTreeNode
   {
+    public virtual FilterTreeNode Normalize()
+    {
+      return this;
+    }
   }
 }
