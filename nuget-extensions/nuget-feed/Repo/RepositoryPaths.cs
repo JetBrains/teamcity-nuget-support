9e49a067b74860eb9411c5862cac3398c96a333a
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-10-19 18:53:31 +0200

    minor changes

--- nuget-extensions/nuget-feed/Repo/RepositoryPaths.cs
+++ nuget-extensions/nuget-feed/Repo/RepositoryPaths.cs
@@ -6,13 +6,11 @@ namespace JetBrains.TeamCity.NuGet.Feed.Repo
 {
   public class RepositoryPaths : IRepositoryPaths
   {
-    [CanBeNull]
     public string TeamCityPackagesFile
     {
       get { return WebConfigurationManager.AppSettings["PackagesSpecFile"]; }
     }
 
-    [CanBeNull]
     public string PackageFilesBasePath
     {
       get { return WebConfigurationManager.AppSettings["PackageFilesBasePath"] ?? HostingEnvironment.MapPath("~"); }
