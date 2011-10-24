0292f4cfa0d95a0e805975a5e7cfac27d6d3cd2d
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-10-24 17:51:33 +0200

    polish test code

--- nuget-tests/src/jetbrains/buildServer/nuget/tests/integration/NuGetServerIntegrationTest.java
+++ nuget-tests/src/jetbrains/buildServer/nuget/tests/integration/NuGetServerIntegrationTest.java
@@ -5,7 +5,6 @@ import jetbrains.buildServer.nuget.server.exec.NuGetTeamCityProvider;
 import jetbrains.buildServer.nuget.server.exec.impl.NuGetExecutorImpl;
 import jetbrains.buildServer.nuget.server.feed.impl.FeedGetMethodFactory;
 import jetbrains.buildServer.nuget.server.feed.impl.FeedHttpClientHolder;
-import jetbrains.buildServer.nuget.server.feed.reader.FeedPackage;
 import jetbrains.buildServer.nuget.server.feed.reader.impl.NuGetFeedReaderImpl;
 import jetbrains.buildServer.nuget.server.feed.reader.impl.PackagesFeedParserImpl;
 import jetbrains.buildServer.nuget.server.feed.reader.impl.Param;
@@ -41,6 +40,8 @@ import org.testng.annotations.Test;
 import java.io.*;
 import java.util.*;
 
+import static org.apache.http.HttpStatus.SC_OK;
+
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
  *         Date: 21.10.11 17:38
@@ -49,7 +50,14 @@ public class NuGetServerIntegrationTest extends BaseTestCase {
   private Mockery m;
   private Collection<InputStream> myStreams;
   private NuGetTeamCityProvider myProvider;
-
+  private File myLogsDir;
+  private FeedHttpClientHolder nyHttpClient;
+  private FeedGetMethodFactory myHttpMethods;
+  private NuGetFeedReaderImpl myFeedReader;
+  private SimpleHttpServer myHttpServer;
+  private NuGetServerRunner myNuGetServer;
+  private String myHttpServerUrl;
+  private String myNuGetServerUrl;
 
   @BeforeMethod
   @Override
@@ -58,9 +66,17 @@ public class NuGetServerIntegrationTest extends BaseTestCase {
     m = new Mockery();
     myStreams = new ArrayList<InputStream>();
     myProvider = m.mock(NuGetTeamCityProvider.class);
+    myLogsDir = createTempDir();
 
-    m.checking(new Expectations(){{
-      allowing(myProvider).getNuGetServerRunnerPath(); will(returnValue(Paths.getNuGetServerRunnerPath()));
+    nyHttpClient = new FeedHttpClientHolder();
+    myHttpMethods = new FeedGetMethodFactory();
+    myFeedReader = new NuGetFeedReaderImpl(nyHttpClient, new UrlResolverImpl(nyHttpClient, myHttpMethods), myHttpMethods, new PackagesFeedParserImpl());
+
+    System.out.println("NuGet server LogsDir = " + myLogsDir);
+
+    m.checking(new Expectations() {{
+      allowing(myProvider).getNuGetServerRunnerPath();
+      will(returnValue(Paths.getNuGetServerRunnerPath()));
     }});
   }
 
@@ -71,25 +87,18 @@ public class NuGetServerIntegrationTest extends BaseTestCase {
     for (InputStream stream : myStreams) {
       FileUtil.close(stream);
     }
-  }
 
-  @NotNull
-  private BuildArtifact artifact(@NotNull final File file) throws IOException {
-    final BuildArtifact a = m.mock(BuildArtifact.class, file.getPath());
-    m.checking(new Expectations(){{
-      allowing(a).getInputStream(); will(new CustomAction("open file") {
-        public Object invoke(Invocation invocation) throws Throwable {
-          final FileInputStream stream = new FileInputStream(file);
-          myStreams.add(stream);
-          return stream;
-        }
-      });
-      allowing(a).getTimestamp(); will(returnValue(file.lastModified()));
-      allowing(a).getSize(); will(returnValue(file.length()));
-      allowing(a).getRelativePath(); will(returnValue(file.getPath()));
-      allowing(a).getName(); will(returnValue(file.getName()));
-    }});
-    return a;
+    for (File file : myLogsDir.listFiles()) {
+      System.out.println("File " + file + ": \r\n" + loadAllText(file));
+    }
+
+    if (myHttpServer != null) {
+      myHttpServer.stop();
+    }
+
+    if (myNuGetServer != null) {
+      myNuGetServer.stopServer();
+    }
   }
 
 
@@ -97,122 +106,127 @@ public class NuGetServerIntegrationTest extends BaseTestCase {
   public void testServerFeed() throws IOException, PackageLoadException, InterruptedException {
     enableDebug();
 
-    final File logsDir = createTempDir();
+    final String packageId = "CommonServiceLocator";
+    final String key = packageId + ".1.0.nupkg";
+    final File responseFile = createTempFile();
 
-    System.out.println("NuGet server LogsDir = " + logsDir);
+    renderResponseFile(key, responseFile);
+    startSimpleHttpServer(responseFile);
+    startNuGetServer();
+
+    assert200("/Packages()").run();
+    assert200("/////Packages()").run();
+    assert200("/Packages()", new Param("$filter", "Id eq '" + packageId + "'")).run();
+    assert200("////Packages()", new Param("$filter", "Id eq '" + packageId + "'")).run();
+
+    Assert.assertTrue(myFeedReader.queryPackageVersions(myNuGetServerUrl + "/", packageId).size() > 0);
+  }
+
+  private void startNuGetServer() {
+    final NuGetServerRunnerSettings settings = m.mock(NuGetServerRunnerSettings.class);
+    m.checking(new Expectations() {{
+      allowing(settings).getPackagesControllerUrl(); will(returnValue(myHttpServerUrl));
+      allowing(settings).getLogsPath(); will(returnValue(myLogsDir));
+    }});
+
+    myNuGetServer = new NuGetServerRunner(settings, new NuGetExecutorImpl(myProvider));
+    myNuGetServer.startServer();
+    NuGetServerUriImpl uri = new NuGetServerUriImpl(myNuGetServer);
+
+    myNuGetServerUrl = uri.getNuGetFeedBaseUri();
+    System.out.println("Created nuget server at: " + myNuGetServerUrl);
+  }
+
+  private void startSimpleHttpServer(@NotNull final File responseFile) throws IOException {
+    final String prefixPath = "/packages";
+
+    myHttpServer = new SimpleHttpServer() {
+      @Override
+      protected Response getResponse(String request) {
+        if (prefixPath.equals(getRequestPath(request))) {
+          return getFileResponse(responseFile, Arrays.asList("Content-Type: text/plain; encoding=UTF-8", "Content-Encoding: utf-8"));
+        }
+        return super.getResponse(request);
+      }
+    };
+    myHttpServer.start();
+    myHttpServerUrl = "http://localhost:" + myHttpServer.getPort() + prefixPath;
+    System.out.println("Created http server at: " + myHttpServerUrl);
+  }
+
+  private void renderResponseFile(@NotNull final String key,
+                                  @NotNull final File responseFile) throws PackageLoadException, IOException {
+    final File packageFile = Paths.getTestDataPath("packages/" + key);
 
     final SBuildType buildType = m.mock(SBuildType.class);
     final BuildsManager buildsManager = m.mock(BuildsManager.class);
     final SFinishedBuild build = m.mock(SFinishedBuild.class);
+    final BuildArtifact artifact = m.mock(BuildArtifact.class, packageFile.getPath());
+    final ArtifactsMetadataEntry entry = m.mock(ArtifactsMetadataEntry.class);
 
-   m.checking(new Expectations(){{
+    final LocalNuGetPackageItemsFactory factory = new LocalNuGetPackageItemsFactory();
+
+
+    m.checking(new Expectations() {{
       allowing(build).getBuildId(); will(returnValue(42L));
-      allowing(build).getBuildTypeId(); will(returnValue("bt"));
-      allowing(build).getBuildType(); will(returnValue(buildType));
+      allowing(build).getBuildTypeId();  will(returnValue("bt"));
+      allowing(build).getBuildType();  will(returnValue(buildType));
       allowing(build).getBuildTypeName(); will(returnValue("buidldzzz"));
       allowing(build).getFinishDate(); will(returnValue(new Date(1319214849319L)));
       allowing(buildType).getLastChangesFinished(); will(returnValue(null));
       allowing(buildsManager).findBuildInstanceById(42); will(returnValue(build));
+
+      allowing(artifact).getInputStream();
+      will(new CustomAction("open file") {
+        public Object invoke(Invocation invocation) throws Throwable {
+          final FileInputStream stream = new FileInputStream(packageFile);
+          myStreams.add(stream);
+          return stream;
+        }
+      });
+
+      allowing(artifact).getTimestamp(); will(returnValue(packageFile.lastModified()));
+      allowing(artifact).getSize(); will(returnValue(packageFile.length()));
+      allowing(artifact).getRelativePath(); will(returnValue(packageFile.getPath()));
+      allowing(artifact).getName(); will(returnValue(packageFile.getName()));
     }});
 
-    final LocalNuGetPackageItemsFactory factory = new LocalNuGetPackageItemsFactory();
-    final String packageId = "CommonServiceLocator";
-    final String key = packageId + ".1.0.nupkg";
-    final Map<String, String> map = factory.loadPackage(artifact(Paths.getTestDataPath("packages/" + key)));
-
-    final ArtifactsMetadataEntry entry = m.mock(ArtifactsMetadataEntry.class);
+    final Map<String, String> map = factory.loadPackage(artifact);
     m.checking(new Expectations(){{
       allowing(entry).getBuildId(); will(returnValue(build.getBuildId()));
       allowing(entry).getKey(); will(returnValue(key));
       allowing(entry).getMetadata(); will(returnValue(Collections.unmodifiableMap(map)));
     }});
 
-    final File temp = createTempFile();
-    Writer w = new OutputStreamWriter(new FileOutputStream(temp), "utf-8");
+    Writer w = new OutputStreamWriter(new FileOutputStream(responseFile), "utf-8");
     w.append("                 ");
     new PackageWriterImpl(buildsManager).serializePackage(entry, w);
     w.append("                  ");
     FileUtil.close(w);
+    System.out.println("Generated response file: " + responseFile);
 
-    System.out.println("Generated response file: " + temp);
-
-    String text = loadAllText(temp);
+    String text = loadAllText(responseFile);
     System.out.println("Generated server response:\r\n" + text);
-
-
-    SimpleHttpServer server = new SimpleHttpServer(){
-      @Override
-      protected Response getResponse(String request) {
-        if ("/packages".equals(getRequestPath(request))) {
-          return getFileResponse(temp, Arrays.asList("Content-Type: text/plain; encoding=UTF-8", "Content-Encoding: utf-8"));
-        }
-        return super.getResponse(request);
-      }
-    };
-    server.start();
-    NuGetServerRunner runner = null;
-    try {
-      final String url = "http://localhost:" + server.getPort() + "/packages";
-      final NuGetServerRunnerSettings settings = m.mock(NuGetServerRunnerSettings.class);
-      m.checking(new Expectations(){{
-        allowing(settings).getPackagesControllerUrl(); will(returnValue(url));
-        allowing(settings).getLogsPath(); will(returnValue(logsDir));
-      }});
-
-      runner = new NuGetServerRunner(settings, new NuGetExecutorImpl(myProvider));
-      runner.startServer();
-      NuGetServerUriImpl uri = new NuGetServerUriImpl(runner);
-
-      final String feedUrl = uri.getNuGetFeedBaseUri();
-      System.out.println("Created http server at: " + url);
-      System.out.println("Created nuget server at: " + feedUrl);
-
-      final FeedHttpClientHolder client = new FeedHttpClientHolder();
-      final FeedGetMethodFactory methods = new FeedGetMethodFactory();
-      NuGetFeedReaderImpl reader = new NuGetFeedReaderImpl(client, new UrlResolverImpl(client, methods), methods, new PackagesFeedParserImpl());
-
-      dumpRequest(feedUrl, client, methods, "/Packages()").run();
-      dumpRequest(feedUrl, client, methods, "/////Packages()").run();
-      dumpRequest(feedUrl, client, methods, "/Packages()",new Param("$filter", "Id eq '" + packageId + "'")).run();
-      dumpRequest(feedUrl, client, methods, "////Packages()",new Param("$filter", "Id eq '" + packageId + "'")).run();
-
-      final Collection<FeedPackage> packages = reader.queryPackageVersions(feedUrl + "/", packageId);
-      Assert.assertTrue(packages.size() > 0);
-
-      System.out.println("Packages: " + packages);
-
-    } finally {
-      for (File file : logsDir.listFiles()) {
-        System.out.println("File " + file + ": \r\n" + loadAllText(file));
-      }
-
-      if (runner != null) {
-        runner.stopServer();
-      }
-
-      server.stop();
-    }
   }
 
   private String loadAllText(File temp) throws IOException {
     return new String(FileUtil.loadFileText(temp, "utf-8"));
   }
 
-  private Runnable dumpRequest(final String feedUrl,
-                               final FeedHttpClientHolder client,
-                               final FeedGetMethodFactory methods,
-                               final String req,
-                               final NameValuePair... reqs) throws IOException {
+  private Runnable assert200(@NotNull final String req,
+                             @NotNull final NameValuePair... reqs) throws IOException {
     return new Runnable() {
       public void run() {
-        final HttpGet get = methods.createGet(feedUrl + req, reqs);
+        final HttpGet get = myHttpMethods.createGet(myNuGetServerUrl + req, reqs);
         try {
-          final HttpResponse execute = client.execute(get);
+          final HttpResponse execute = nyHttpClient.execute(get);
           final HttpEntity entity = execute.getEntity();
           System.out.println("Request: " + get.getRequestLine());
           entity.writeTo(System.out);
           System.out.println();
           System.out.println();
+
+          Assert.assertTrue(execute.getStatusLine().getStatusCode() == SC_OK);
         } catch (IOException e) {
           ExceptionUtil.rethrowAsRuntimeException(e);
         } finally {
