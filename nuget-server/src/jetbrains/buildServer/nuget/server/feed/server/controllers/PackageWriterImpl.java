8e199bbbfb95d7bdcc137774b6b5aa62bb8e8464
Eugene Petrenko <eugene.petrenko@gmail.com>
2011-11-24 11:32:13 +0100

    avoid getting SBuild for each package metadata entry

--- nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/controllers/PackageWriterImpl.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/controllers/PackageWriterImpl.java
@@ -17,10 +17,7 @@
 package jetbrains.buildServer.nuget.server.feed.server.controllers;
 
 import jetbrains.buildServer.nuget.server.feed.server.PackagesIndex;
-import jetbrains.buildServer.serverSide.BuildsManager;
-import jetbrains.buildServer.serverSide.SBuild;
-import jetbrains.buildServer.serverSide.SBuildType;
-import jetbrains.buildServer.serverSide.SFinishedBuild;
+import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.serverSide.metadata.BuildMetadataEntry;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,41 +26,60 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
  *         Date: 21.10.11 16:58
  */
 public class PackageWriterImpl implements PackagesWriter {
-  @NotNull private final BuildsManager myBuilds;
-  @NotNull private final PackagesIndex myIndex;
-  @NotNull private final PackageInfoSerializer mySerializer;
+  @NotNull
+  private final PackagesIndex myIndex;
+  @NotNull
+  private final PackageInfoSerializer mySerializer;
+  @NotNull
+  private final ProjectManager myProjectManager;
 
-  public PackageWriterImpl(@NotNull final BuildsManager builds,
-                           @NotNull final PackagesIndex index,
-                           @NotNull final PackageInfoSerializer serializer) {
-    myBuilds = builds;
+  public PackageWriterImpl(@NotNull final PackagesIndex index,
+                           @NotNull final PackageInfoSerializer serializer,
+                           @NotNull final ProjectManager projectManager) {
+    myProjectManager = projectManager;
     myIndex = index;
     mySerializer = serializer;
   }
 
-  private  void serializePackage(@NotNull final BuildMetadataEntry entry,
-                                 @NotNull final Writer writer) throws IOException {
+  private class LatestBuildsCache {
+    private final Map<String, Long> myBuildTypeToLatest = new HashMap<String, Long>();
 
-    final SBuild aBuild = myBuilds.findBuildInstanceById(entry.getBuildId());
-    if (aBuild == null || !(aBuild instanceof SFinishedBuild)) return;
-    final SFinishedBuild build = (SFinishedBuild) aBuild;
+    public Boolean isLatest(@NotNull final String buildTypeId, final long buildId) {
+      Long build = myBuildTypeToLatest.get(buildTypeId);
+      if (build == null) {
+        final SBuildType buildTypeById = myProjectManager.findBuildTypeById(buildTypeId);
+        //skip project if no build type found
+        if (buildTypeById == null) return null;
 
-    final SBuildType buildType = build.getBuildType();
-    final SFinishedBuild latestBuild = buildType == null ? null : buildType.getLastChangesFinished();
-    final boolean isLatestVersion = latestBuild == null || latestBuild.getBuildId() == build.getBuildId();
+        final SFinishedBuild lastChangesFinished = buildTypeById.getLastChangesFinished();
+        //no latest build found, than skip this build
+        if (lastChangesFinished == null) return null;
+
+        myBuildTypeToLatest.put(buildTypeId, build = lastChangesFinished.getBuildId());
+      }
+      return build == buildId;
+    }
+  }
+
+  private void serializePackage(@NotNull final LatestBuildsCache cache,
+                                @NotNull final BuildMetadataEntry entry,
+                                @NotNull final Writer writer) throws IOException {
+
+    final String buildTypeId = entry.getMetadata().get(PackagesIndex.TEAMCITY_BUILD_TYPE_ID);
+    //skip older entries.
+    if (buildTypeId == null) return;
+    final boolean isLatestVersion = cache.isLatest(buildTypeId, entry.getBuildId());
 
     mySerializer.serializePackage(
             entry.getMetadata(),
-            entry.getMetadata().get(PackagesIndex.TEAMCITY_BUILD_TYPE_ID),
+            buildTypeId,
             entry.getBuildId(),
             isLatestVersion,
             writer
@@ -73,18 +89,16 @@ public class PackageWriterImpl implements PackagesWriter {
   public void serializePackages(@NotNull final HttpServletRequest request,
                                 @NotNull final HttpServletResponse response) throws IOException {
     final PrintWriter writer = response.getWriter();
-
     final Set<String> reportedPackages = new HashSet<String>();
-
     final Iterator<BuildMetadataEntry> entries = myIndex.getEntries();
+
+    final LatestBuildsCache cache = new LatestBuildsCache();
 
     while (entries.hasNext()) {
       final BuildMetadataEntry e = entries.next();
       //remove duplicates
       if (!reportedPackages.add(e.getKey())) continue;
-
-      serializePackage(e, writer);
-
+      serializePackage(cache, e, writer);
       writer.write("\r\n");
     }
     writer.flush();
