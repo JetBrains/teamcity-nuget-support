fade63e720a39cc27b49ce77d29465bb9f7aa9c8
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-02-08 21:04:20 +0400

    add packages folder setting, make it work somehow

--- standalone-server/src/jetbrains/buildServer/nuget/standalone/server/NuGetApplicationPackagesIndex.java
+++ standalone-server/src/jetbrains/buildServer/nuget/standalone/server/NuGetApplicationPackagesIndex.java
@@ -16,33 +16,79 @@
 
 package jetbrains.buildServer.nuget.standalone.server;
 
-import jetbrains.buildServer.nuget.server.feed.server.index.impl.NuGetPackageBuilder;
-import jetbrains.buildServer.nuget.server.feed.server.index.impl.PackagesIndexBase;
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.nuget.server.feed.server.index.impl.*;
 import jetbrains.buildServer.nuget.server.feed.server.index.impl.transform.IsLatestCalculator;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
  * Date: 29.01.12 23:42
  */
 public class NuGetApplicationPackagesIndex extends PackagesIndexBase<Entry> {
+  private static final Logger LOG = Logger.getInstance(NuGetApplicationPackagesIndex.class.getName());
+
   @NotNull
   @Override
   protected Iterator<Entry> getEntries() {
-    return Collections.<Entry>emptyList().iterator();
+    int id = 42;
+    final LocalNuGetPackageItemsFactory factory = new LocalNuGetPackageItemsFactory();
+    final File[] file = Main.getPackagesRoot().listFiles();
+    if (file == null) return Collections.<Entry>emptyList().iterator();
+    final List<Entry> result = new ArrayList<Entry>();
+    for (final File ch : file) {
+      if (ch.isFile() && ch.getName().endsWith(".nupkg")) {
+        final Map<String, String> map = loadPackage(factory, ch);
+        if (map != null) {
+          result.add(new Entry(++id, map));
+        }
+      }
+    }
+    return result.iterator();
+  }
+
+  @Nullable
+  private Map<String, String> loadPackage(LocalNuGetPackageItemsFactory factory, final File ch) {
+    try {
+      return factory.loadPackage(file(ch));
+    } catch (PackageLoadException e) {
+      LOG.warn("Failed to load package: " + ch);
+      return null;
+    }
+  }
+
+  private PackageFile file(final File ch) {
+    return new PackageFile() {
+      public long getSize() {
+        return ch.length();
+      }
+
+      @NotNull
+      public Date getLastUpdated() {
+        return new Date(ch.lastModified());
+      }
+
+      @NotNull
+      public InputStream getInputStream() throws IOException {
+        return new BufferedInputStream(new FileInputStream(ch));
+      }
+    };
   }
 
   @Override
   protected NuGetPackageBuilder builderFromEntry(@NotNull Entry entry) {
-    return new NuGetPackageBuilder(entry.getKey(), entry.getId(), entry.getMap());
+    final NuGetPackageBuilder builder = new NuGetPackageBuilder(entry.getKey(), entry.getId(), entry.getMap());
+    builder.setBuildTypeId("not-tc");
+    return builder;
   }
 
   @Override
   protected String getDownloadUrl(@NotNull NuGetPackageBuilder builder) {
-    return null;
+    return "/download/" + builder.getKey();
   }
 
   @NotNull
@@ -50,7 +96,8 @@ public class NuGetApplicationPackagesIndex extends PackagesIndexBase<Entry> {
   protected IsLatestCalculator createIsLatestTransformation() {
     return new IsLatestCalculator() {
       public Boolean isLatest(@NotNull NuGetPackageBuilder builder) {
-        return null;
+        //TODO: implement is latest computation
+        return false;
       }
     };
   }
