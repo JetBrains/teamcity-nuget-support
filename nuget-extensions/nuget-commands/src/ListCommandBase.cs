4867b967af78cfd4dd0c3cdd24b642711f398f65
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-05-11 17:15:18 +0200

    drop legacy commands

--- nuget-extensions/nuget-commands/src/ListCommandBase.cs
+++ nuget-extensions/nuget-commands/src/ListCommandBase.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Linq.Expressions;
 using JetBrains.Annotations;
-using JetBrains.TeamCity.NuGet.ExtendedCommands.Util;
 using NuGet;
 
 namespace JetBrains.TeamCity.NuGet.ExtendedCommands
@@ -90,21 +89,6 @@ namespace JetBrains.TeamCity.NuGet.ExtendedCommands
 
       return expressions.Single();
     }
-
-    /// <summary>
-    /// There was a change in signature in NuGet poset 1.5. IPackage.get_Version now returns Semantic version instead of Version
-    /// Here is no difference here, so we can stick to dynamic object as there are tests for this method.
-    /// </summary>    
-    protected static void PrintPackageInfo(string id, string version)
-    {    
-      var msg = ServiceMessageFormatter.FormatMessage(
-        "nuget-package",
-        new ServiceMessageProperty("Id", id),
-        new ServiceMessageProperty("Version", version)
-        );
-
-      System.Console.Out.WriteLine(msg);
-    }
   }
 
   public enum PackageFetchOption
@@ -118,7 +102,9 @@ namespace JetBrains.TeamCity.NuGet.ExtendedCommands
   {
     public static string VersionString(this IPackage package)
     {
-      return ((dynamic) package).Version.ToString(); 
+      // There was a change in signature in NuGet poset 1.5. IPackage.get_Version now returns Semantic version instead of Version
+      // Here is no difference here, so we can stick to dynamic object as there are tests for this method.
+      return ((dynamic)package).Version.ToString(); 
     }
   }
 }
