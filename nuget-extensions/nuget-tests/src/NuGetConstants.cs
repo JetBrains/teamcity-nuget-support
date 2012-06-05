27030837c32fb7c25bc8b411e6931afb66af98a8
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-06-05 15:55:11 +0200

    improve fetch of Latest NuGet

--- nuget-extensions/nuget-tests/src/NuGetConstants.cs
+++ nuget-extensions/nuget-tests/src/NuGetConstants.cs
@@ -4,5 +4,6 @@ namespace JetBrains.TeamCity.NuGet.Tests
   {
     public const string DefaultFeedUrl_v1 = "https://go.microsoft.com/fwlink/?LinkID=206669";
     public const string DefaultFeedUrl_v2 = "https://go.microsoft.com/fwlink/?LinkID=230477";
+    public const string NuGetFeed = "http://nuget.org/api/v2/";
   }
 }
