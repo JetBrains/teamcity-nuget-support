e09d015b199e00324f29e1793b3390a54bbff922
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-06-16 00:51:43 +0200

    parse .sln file with regex

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/util/sln/impl/SolutionParserImpl.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/util/sln/impl/SolutionParserImpl.java
@@ -16,12 +16,17 @@
 
 package jetbrains.buildServer.nuget.agent.util.sln.impl;
 
+import com.intellij.openapi.util.io.StreamUtil;
 import jetbrains.buildServer.nuget.agent.util.sln.SolutionFileParser;
+import jetbrains.buildServer.util.FileUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Collections;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -29,7 +34,32 @@ import java.util.Collections;
  */
 public class SolutionParserImpl implements SolutionFileParser {
   @NotNull
-  public Collection<File> parseProjectFiles(@NotNull File sln) {
-    return Collections.emptyList();
+  public Collection<File> parseProjectFiles(@NotNull final File sln) throws IOException {
+    final File root = sln.getParentFile();
+    if (root == null) Collections.emptyList();
+
+    final Collection<String> lines = readFileText(sln);
+    final Pattern pt = Pattern.compile("^\\s*Project\\(\".*\"\\)\\s*");
+
+    final List<File> files = new ArrayList<File>();
+    for (String line : lines) {
+      final Matcher matcher = pt.matcher(line);
+      if (matcher.matches()) {
+        files.add(new File(root, matcher.group(1)));
+      }
+    }
+
+    return Collections.unmodifiableList(files);
+  }
+
+  @NotNull
+  private Collection<String> readFileText(@NotNull final File file) throws IOException {
+    final FileInputStream is = new FileInputStream(file);
+    try {
+      final String text = StreamUtil.readText(is, "utf-8");
+      return Arrays.asList(text.split("[\\r\\n]+"));
+    } finally {
+      FileUtil.close(is);
+    }
   }
 }
