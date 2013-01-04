795d0d9eca8815161e7ffca6974dfae318268844
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-01-04 19:03:52 +0100

    extract class to standalone file

--- nuget-extensions/nuget-commands/src/NuGetTeamCityPingCommand.cs
+++ nuget-extensions/nuget-commands/src/NuGetTeamCityPingCommand.cs
@@ -5,22 +5,6 @@ using NuGet.Commands;
 
 namespace JetBrains.TeamCity.NuGet.ExtendedCommands
 {
-  [Export]
-  public class NuGetTeamCityInfo : ICreatableComponent
-  {
-    public void Initialize()
-    {
-      LogRuntimeInfo();
-    }
-
-    public void LogRuntimeInfo()
-    {
-      System.Console.Out.WriteLine("TeamCity NuGet Extension is available.");
-      System.Console.Out.WriteLine("NuGet Version = {0}", typeof(Command).Assembly.GetName().Version);
-      System.Console.Out.WriteLine("TeamCity Extension = {0}", GetType().Assembly.GetName().FullName);
-    }
-  }
-
   [Command("TeamCity.Ping", "Command that dump NuGet and TeamCity extension versions. It is used to check NuGet<->TeamCity communications")]
   public class NuGetTeamCityPingCommand : CommandBase
   {
