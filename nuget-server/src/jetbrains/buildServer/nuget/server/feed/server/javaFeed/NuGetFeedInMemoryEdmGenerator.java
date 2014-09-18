936890d2715a6d2974124431a2fbdccdf5d6ba69
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-09-18 16:21:06 +0400

    function import parameters and return types specified

--- nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/javaFeed/NuGetFeedInMemoryEdmGenerator.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/javaFeed/NuGetFeedInMemoryEdmGenerator.java
@@ -16,54 +16,104 @@
 
 package jetbrains.buildServer.nuget.server.feed.server.javaFeed;
 
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.util.CollectionsUtil;
+import jetbrains.buildServer.util.filters.Filter;
+import org.jetbrains.annotations.NotNull;
 import org.odata4j.edm.*;
 import org.odata4j.producer.inmemory.InMemoryEdmGenerator;
 import org.odata4j.producer.inmemory.InMemoryEntityInfo;
 import org.odata4j.producer.inmemory.InMemoryTypeMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Evgeniy.Koshkin
  */
 public class NuGetFeedInMemoryEdmGenerator extends InMemoryEdmGenerator {
-  private EdmFunctionImport.Builder mySearchFunc;
-  private EdmFunctionImport.Builder myFindPackagesByIdFunc;
-  private EdmFunctionImport.Builder myGetUpdatesFunc;
+
+  private static final String SEARCH_TERM = "searchTerm";
+  private static final String TARGET_FRAMEWORK = "targetFramework";
+  private static final String INCLUDE_PRERELEASE = "includePrerelease";
+  private static final String PACKAGE_IDS = "packageIds";
+  private static final String VERSIONS = "versions";
+  private static final String INCLUDE_ALL_VERSIONS = "includeAllVersions";
+  private static final String TARGET_FRAMEWORKS = "targetFrameworks";
+  private static final String VERSION_CONSTRAINTS = "versionConstraints";
+  private static final String ID = "id";
+
+  private final Logger LOG = Logger.getInstance(getClass().getName());
 
   public NuGetFeedInMemoryEdmGenerator(String namespace, String containerName, InMemoryTypeMapping typeMapping,
                               String idPropertyName, Map<String, InMemoryEntityInfo<?>> eis) {
     super(namespace, containerName, typeMapping, idPropertyName, eis);
-
-    final EdmEntitySet.Builder packagesEntitySet = new EdmEntitySet.Builder().setName(MetadataConstants.ENTITY_SET_NAME);
-
-    mySearchFunc = new EdmFunctionImport.Builder()
-            .setName(MetadataConstants.SEARCH_FUNCTION_NAME)
-            .setEntitySet(packagesEntitySet)
-            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET);
-
-    myFindPackagesByIdFunc = new EdmFunctionImport.Builder()
-            .setName(MetadataConstants.FIND_PACKAGES_BY_ID_FUNCTION_NAME)
-            .setEntitySet(packagesEntitySet)
-            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET);
-
-    myGetUpdatesFunc = new EdmFunctionImport.Builder()
-            .setName(MetadataConstants.GET_UPDATES_FUNCTION_NAME)
-            .setEntitySet(packagesEntitySet)
-            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET);
   }
 
   @Override
   public EdmDataServices.Builder generateEdm(EdmDecorator decorator) {
     final EdmDataServices.Builder edmBuilder = super.generateEdm(decorator);
+    boolean setFuncImports = false;
     for(EdmSchema.Builder schemaBuilder : edmBuilder.getSchemas()){
       if(schemaBuilder.getNamespace().equalsIgnoreCase(MetadataConstants.NUGET_GALLERY_NAMESPACE)){
-        for(EdmEntityContainer.Builder entityContainerBuilder : schemaBuilder.getEntityContainers()){
-          if(entityContainerBuilder.getName().equalsIgnoreCase(MetadataConstants.CONTAINER_NAME))
-          entityContainerBuilder.addFunctionImports(mySearchFunc, myFindPackagesByIdFunc, myGetUpdatesFunc);
+        final EdmEntityType.Builder entityTypeBuilder = CollectionsUtil.findFirst(schemaBuilder.getEntityTypes(), new Filter<EdmEntityType.Builder>() {
+          public boolean accept(@NotNull EdmEntityType.Builder data) {
+            return data.getName().equalsIgnoreCase(MetadataConstants.ENTITY_TYPE_NAME);
+          }
+        });
+        if(entityTypeBuilder != null) {
+          for(EdmEntityContainer.Builder entityContainerBuilder : schemaBuilder.getEntityContainers()){
+            if(entityContainerBuilder.getName().equalsIgnoreCase(MetadataConstants.CONTAINER_NAME)){
+              entityContainerBuilder.addFunctionImports(generateNugetAPIv2FunctionImports(entityTypeBuilder.build()));
+              setFuncImports = true;
+            }
+          }
         }
       }
     }
+
+    if(setFuncImports)
+      LOG.debug("NuGet API v2 function imports were setted up succesfully.");
+    else
+      LOG.warn("NuGet API v2 function imports were NOT setted up.");
+
     return edmBuilder;
+  }
+
+  private List<EdmFunctionImport.Builder> generateNugetAPIv2FunctionImports(EdmEntityType entityType) {
+    final EdmEntitySet.Builder packagesEntitySet = new EdmEntitySet.Builder().setName(MetadataConstants.ENTITY_SET_NAME);
+    final EdmType packagesCollectionType = new EdmCollectionType(EdmProperty.CollectionKind.Collection, entityType);
+    final List<EdmFunctionImport.Builder> result = new ArrayList<EdmFunctionImport.Builder>();
+
+    result.add(new EdmFunctionImport.Builder()
+            .setName(MetadataConstants.SEARCH_FUNCTION_NAME)
+            .setEntitySet(packagesEntitySet)
+            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET)
+            .setReturnType(packagesCollectionType)
+            .addParameters(new EdmFunctionParameter.Builder().setName(SEARCH_TERM).setType(EdmSimpleType.STRING),
+                    new EdmFunctionParameter.Builder().setName(TARGET_FRAMEWORK).setType(EdmSimpleType.STRING),
+                    new EdmFunctionParameter.Builder().setName(INCLUDE_PRERELEASE).setType(EdmSimpleType.BOOLEAN)));
+
+    result.add(new EdmFunctionImport.Builder()
+            .setName(MetadataConstants.FIND_PACKAGES_BY_ID_FUNCTION_NAME)
+            .setEntitySet(packagesEntitySet)
+            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET)
+            .setReturnType(packagesCollectionType)
+            .addParameters(new EdmFunctionParameter.Builder().setName(ID).setType(EdmSimpleType.STRING)));
+
+    result.add(new EdmFunctionImport.Builder()
+            .setName(MetadataConstants.GET_UPDATES_FUNCTION_NAME)
+            .setEntitySet(packagesEntitySet)
+            .setHttpMethod(MetadataConstants.HTTP_METHOD_GET)
+            .setReturnType(packagesCollectionType)
+            .addParameters(new EdmFunctionParameter.Builder().setName(PACKAGE_IDS).setType(EdmSimpleType.STRING),
+                    new EdmFunctionParameter.Builder().setName(VERSIONS).setType(EdmSimpleType.STRING),
+                    new EdmFunctionParameter.Builder().setName(INCLUDE_PRERELEASE).setType(EdmSimpleType.BOOLEAN),
+                    new EdmFunctionParameter.Builder().setName(INCLUDE_ALL_VERSIONS).setType(EdmSimpleType.BOOLEAN),
+                    new EdmFunctionParameter.Builder().setName(TARGET_FRAMEWORKS).setType(EdmSimpleType.STRING),
+                    new EdmFunctionParameter.Builder().setName(VERSION_CONSTRAINTS).setType(EdmSimpleType.STRING)));
+
+    return result;
   }
 }
