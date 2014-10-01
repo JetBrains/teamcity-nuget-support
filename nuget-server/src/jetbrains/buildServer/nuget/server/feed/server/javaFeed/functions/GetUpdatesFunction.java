7a72725931ff9ebd3d302fee4c091045c2aaceb1
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-10-01 12:11:12 +0400

    dummy GetUpdates function implemented; packageIds, versions, includeAllVersions parameters handled correctly

--- nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/javaFeed/functions/GetUpdatesFunction.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/feed/server/javaFeed/functions/GetUpdatesFunction.java
@@ -16,20 +16,38 @@
 
 package jetbrains.buildServer.nuget.server.feed.server.javaFeed.functions;
 
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.nuget.server.feed.server.index.NuGetIndexEntry;
+import jetbrains.buildServer.nuget.server.feed.server.index.PackagesIndex;
+import jetbrains.buildServer.nuget.server.feed.server.index.impl.SemanticVersionsComparer;
 import jetbrains.buildServer.nuget.server.feed.server.javaFeed.MetadataConstants;
+import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.odata4j.core.OFunctionParameter;
+import org.odata4j.core.OObject;
+import org.odata4j.core.OSimpleObject;
 import org.odata4j.edm.*;
 import org.odata4j.producer.QueryInfo;
-import org.odata4j.producer.exceptions.NotImplementedException;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Evgeniy.Koshkin
  */
 public class GetUpdatesFunction implements NuGetFeedFunction {
+
+  private static final String COLLECTION_VALUE_SEPARATOR = "|";
+  private static final Comparator<String> SEMANTIC_VERSIONS_COMPARATOR = SemanticVersionsComparer.getSemanticVersionsComparator();
+
+  private final Logger LOG = Logger.getInstance(getClass().getName());
+
+  @NotNull private final PackagesIndex myIndex;
+
+  public GetUpdatesFunction(@NotNull PackagesIndex index) {
+    myIndex = index;
+  }
+
   @NotNull
   public String getName() {
     return MetadataConstants.GET_UPDATES_FUNCTION_NAME;
@@ -53,6 +71,77 @@ public class GetUpdatesFunction implements NuGetFeedFunction {
 
   @Nullable
   public org.odata4j.producer.BaseResponse call(@NotNull EdmType returnType, @NotNull Map<String, OFunctionParameter> params, @Nullable QueryInfo queryInfo) {
-    throw new NotImplementedException();
+    final List<String> packageIds = extractListOfStringsFromParamValue(params, MetadataConstants.PACKAGE_IDS);
+    if(packageIds.isEmpty()){
+      //TODO LOG
+      return null;
+    }
+
+    final List<String> versions = extractListOfStringsFromParamValue(params, MetadataConstants.VERSIONS);
+    if(versions.isEmpty()){
+      //TODO LOG
+      return null;
+    }
+
+    if(packageIds.size() != versions.size()){
+      //TODO LOG
+      return null;
+    }
+
+    final boolean includeAllVersions = extractBooleanParameterValue(params, MetadataConstants.INCLUDE_ALL_VERSIONS, false);
+    final List<NuGetIndexEntry> result = new ArrayList<NuGetIndexEntry>();
+
+    for(int i = 0; i < packageIds.size(); i++){
+      final String version = versions.get(i);
+      final String packageId = packageIds.get(i);
+      final Iterator<NuGetIndexEntry> entryIterator = myIndex.getNuGetEntries(packageId);
+      while (entryIterator.hasNext()){
+        final NuGetIndexEntry indexEntry = entryIterator.next();
+        if(SEMANTIC_VERSIONS_COMPARATOR.compare(version, indexEntry.getPackageInfo().getVersion()) < 0){
+          //TODO LOG
+          result.add(indexEntry);
+          if(!includeAllVersions){
+            //TODO LOG
+            break;
+          }
+        }
+      }
+    }
+
+    if(result.isEmpty()){
+      //TODO LOG
+      return null;
+    }
+    return new NuGetPackagesCollectionResponse(result.iterator(), returnType);
+  }
+
+  private boolean extractBooleanParameterValue(Map<String, OFunctionParameter> parameters, String parameterName, boolean defaultValue) {
+    final OFunctionParameter parameter = parameters.get(parameterName);
+    if(parameter == null){
+      //TODO: LOG
+      return defaultValue;
+    }
+    final OObject valueObject = parameter.getValue();
+    if(!(valueObject instanceof OSimpleObject))
+    {
+      //TODO: LOG
+      return defaultValue;
+    }
+    return Boolean.valueOf(((OSimpleObject) valueObject).getValue().toString());
+  }
+
+  private List<String> extractListOfStringsFromParamValue(Map<String, OFunctionParameter> parameters, String parameterName){
+    final OFunctionParameter parameter = parameters.get(parameterName);
+    if(parameter == null){
+      //TODO: LOG
+      return null;
+    }
+    final OObject valueObject = parameter.getValue();
+    if(!(valueObject instanceof OSimpleObject))
+    {
+      //TODO: LOG
+      return null;
+    }
+    return StringUtil.split(((OSimpleObject) valueObject).getValue().toString(), COLLECTION_VALUE_SEPARATOR);
   }
 }
