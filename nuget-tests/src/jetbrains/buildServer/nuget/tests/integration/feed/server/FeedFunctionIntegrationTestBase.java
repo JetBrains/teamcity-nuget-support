f7b24b9e35efdd1c3bdae7fb6e59c4292f1fefd6
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-11-11 11:33:04 +0300

    tests for getupdates function added

---
+++ nuget-tests/src/jetbrains/buildServer/nuget/tests/integration/feed/server/FeedFunctionIntegrationTestBase.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2000-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.nuget.tests.integration.feed.server;
+
+import jetbrains.buildServer.nuget.server.feed.server.javaFeed.NuGetAPIVersion;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * @author Evgeniy.Koshkin
+ */
+public abstract class FeedFunctionIntegrationTestBase extends NuGetJavaFeedIntegrationTestBase {
+  @Override
+  @BeforeMethod
+  public void setUp() throws Exception {
+    super.setUp();
+    System.setProperty(NuGetAPIVersion.TEAMCITY_NUGET_API_VERSION_PROP_NAME, NuGetAPIVersion.V2);
+  }
+
+  @Override
+  @AfterMethod
+  public void tearDown() throws Exception {
+    System.getProperties().remove(NuGetAPIVersion.TEAMCITY_NUGET_API_VERSION_PROP_NAME);
+    super.tearDown();
+  }
+
+  protected void assertContainsPackageVersion(String responseBody, String version){
+    assertContains(responseBody, "<d:Version>" + version + "</d:Version>");
+  }
+
+  protected void assertNotContainsPackageVersion(String responseBody, String version){
+    assertNotContains(responseBody, "<d:Version>" + version + "</d:Version>", false);
+  }
+
+  protected void assertPackageVersionsOrder(String responseBody, String... versions) {
+    int prevVersionPosition = 0;
+    for (String version : versions){
+      final int i = responseBody.indexOf("<d:Version>" + version + "</d:Version>");
+      if(i == -1) fail("Response doesn't contain package version " + version);
+      assertGreater(i, prevVersionPosition);
+      prevVersionPosition = i;
+    }
+  }
+}
