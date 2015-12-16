617d17b2c0889b21af93c06f2d4d107cbcf11870
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2015-12-16 17:18:51 +0300

    fix TW-43449 - add working CredentialProvider implementation

--- nuget-extensions/credential-provider/src/Program.cs
+++ nuget-extensions/credential-provider/src/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using JetBrains.TeamCity.NuGet.ExtendedCommands;
+using JetBrains.TeamCity.NuGet.ExtendedCommands.Data;
 using PowerArgs;
 
 namespace JetBrains.TeamCity.NuGet.CredentialProvider
@@ -21,13 +24,55 @@ namespace JetBrains.TeamCity.NuGet.CredentialProvider
             return (int)ExitCode.UnknownError;
           }
 
+          var response = new CredentialProviderResponse();
+          ExitCode exitCode;
+
+          var path = Environment.GetEnvironmentVariable("TEAMCITY_NUGET_FEEDS");
+          if (string.IsNullOrWhiteSpace(path))
+          {
+            response.Message = "Failed to load NuGet feed credentials file on path " + path;
+            Console.Error.WriteLine("Failed to find TEAMCITY_NUGET_FEEDS var in environment");
+            exitCode = ExitCode.ProviderNotApplicable;
+          }
+          else if (!File.Exists(path))
+          {
+            response.Message = "Failed to load NuGet feed credentials file on path " + path;
+            exitCode = ExitCode.Failure;
+          }
+          else
+          {
+            var sources = XmlSerializerHelper.Load<NuGetSources>(path).Sources.Where(x => x.HasCredentials).ToArray();
+            if (sources.Length == 0)
+            {
+              response.Message = "NuGet feed credentials file contains no sources with credentials specified";
+              exitCode = ExitCode.ProviderNotApplicable;
+            }
+            else
+            {
+              var targetUri = credentialProviderArgs.Uri;
+              var targetSource = sources.FirstOrDefault(source => targetUri.AbsoluteUri.Equals(source.Source));
+              if (targetSource == null)
+              {
+                response.Message = "NuGet feed credentials file contains no credentials for URL " + targetUri;
+                exitCode = ExitCode.ProviderNotApplicable;
+              }
+              else
+              {
+                response.Username = targetSource.Username;
+                response.Password = targetSource.Password;
+                response.Message = "Success";
+                exitCode = ExitCode.Success;
+              }
+            }
+          }
+
           using (var streamWriter = new StreamWriter(Console.OpenStandardOutput()))
           {
             streamWriter.AutoFlush = true;
             Console.SetOut(streamWriter);
-            Console.WriteLine(new CredentialProviderResponse{Message = "message", Password = "pwd", Username = "user"}.ToString());
+            Console.WriteLine(response.ToString());
           }
-          return (int)ExitCode.Failure;
+          return (int)exitCode;
         }
 
       }
--- nuget-extensions/nuget-commands/src/XmlSerializerHelper.cs
+++ nuget-extensions/nuget-commands/src/XmlSerializerHelper.cs
@@ -1,6 +1,6 @@
+using System;
 using System.IO;
 using System.Xml.Serialization;
-using NuGet;
 
 namespace JetBrains.TeamCity.NuGet.ExtendedCommands
 {
@@ -9,13 +9,13 @@ namespace JetBrains.TeamCity.NuGet.ExtendedCommands
     public static T Load<T>(string path)
     {
       if (!File.Exists(path))
-        throw new CommandLineException("Failed to find file at {0}", path);
+        throw new ApplicationException("Failed to find file at " + path);
 
       using (var file = File.OpenRead(path))
       {
         var parser = new XmlSerializerFactory().CreateSerializer(typeof(T));
         if (parser == null)
-          throw new CommandLineException("Failed to create serialized for " + typeof(T).FullName);
+          throw new ApplicationException("Failed to create serialized for " + typeof(T).FullName);
 
         return (T)parser.Deserialize(file);
       }
@@ -24,13 +24,13 @@ namespace JetBrains.TeamCity.NuGet.ExtendedCommands
     public static void Save<T>(string path, T data)
     {
       if (!File.Exists(path))
-        throw new CommandLineException("Failed to find file at {0}", path);
+        throw new ApplicationException("Failed to find file at " + path);
 
       using (var file = File.CreateText(path))
       {
         var parser = new XmlSerializerFactory().CreateSerializer(typeof(T));
         if (parser == null)
-          throw new CommandLineException("Failed to create serialized for " + typeof(T).FullName);
+          throw new ApplicationException("Failed to create serialized for " + typeof(T).FullName);
 
         parser.Serialize(file, data);
       }
