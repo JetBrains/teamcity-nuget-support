016f98fb69c239777cfddeebafe700c273225982
Evgeniy.Koshkin <evgeniy.koshkin@jetbrains.com>
2016-11-22 22:25:16 +0300

    fix spelling

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/publish/PackagesPublishRunner.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/publish/PackagesPublishRunner.java
@@ -59,7 +59,7 @@ public class PackagesPublishRunner extends NuGetRunnerBase {
           @Override
           protected BuildFinishedStatus waitForImpl() throws RunBuildException {
             if(FeedConstants.SYMBOLS_PACKAGE_FILE_FILTER.accept(file)){
-              context.getBuild().getBuildLogger().warning("Attemp to publish symbol package. " +
+              context.getBuild().getBuildLogger().warning("Attempt to publish symbol package. " +
                       "Symbol packages are not fully supported by TeamCity internal feed. " +
                       "For more details see https://confluence.jetbrains.com/display/TCD10/NuGet#NuGet-symbols");
             } else if (!FeedConstants.PACKAGE_FILE_FILTER.accept(file)) {
