9d1e7d01b71d878a69c4268dc2c1785e2540653a
Dmitry.Tretyakov <dtretyakov@gmail.com>
2017-03-15 18:08:30 +0300

    Cleanup nuget feed password parameters provider
    After refactoring in the TeamCity core it's enough to declare parameter
    without build step context.

--- nuget-feed/src/jetbrains/buildServer/nuget/feed/server/NuGetFeedPasswordProvider.java
+++ nuget-feed/src/jetbrains/buildServer/nuget/feed/server/NuGetFeedPasswordProvider.java
@@ -17,20 +17,16 @@
 package jetbrains.buildServer.nuget.feed.server;
 
 import jetbrains.buildServer.nuget.common.NuGetServerConstants;
-import jetbrains.buildServer.nuget.common.PackagesConstants;
-import jetbrains.buildServer.serverSide.BuildPromotionEx;
 import jetbrains.buildServer.serverSide.Parameter;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.SimpleParameter;
-import jetbrains.buildServer.serverSide.build.steps.BuildStepsEditor;
-import jetbrains.buildServer.serverSide.build.steps.StepContext;
-import jetbrains.buildServer.serverSide.impl.SBuildStepsCollection;
-import jetbrains.buildServer.serverSide.impl.build.steps.BuildStartContextBase;
 import jetbrains.buildServer.serverSide.parameters.types.PasswordsProvider;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 /**
@@ -49,20 +45,6 @@ public class NuGetFeedPasswordProvider implements PasswordsProvider {
 
     if (!StringUtil.isEmptyOrSpaces(feedApiKey)) {
       passwords.add(new SimpleParameter(NuGetServerConstants.FEED_REFERENCE_AGENT_API_KEY_PROVIDED, feedApiKey));
-    }
-
-    final BuildPromotionEx buildPromotion = (BuildPromotionEx)build.getBuildPromotion();
-    final SBuildStepsCollection runners = buildPromotion.getBuildSettings().getAllBuildRunners();
-    if (!(runners instanceof BuildStartContextBase)) return passwords;
-
-    final BuildStepsEditor steps = ((BuildStartContextBase)runners).getRootSteps();
-    for (StepContext step : steps.getSteps()) {
-      if (!PackagesConstants.PUBLISH_RUN_TYPE.equals(step.getRunType().getType())) continue;
-
-      final String apiKey = step.getParameters().get(PackagesConstants.NUGET_API_KEY);
-      if (StringUtil.isEmptyOrSpaces(apiKey)) continue;
-
-      passwords.add(new SimpleParameter(PackagesConstants.NUGET_API_KEY, apiKey));
     }
 
     return passwords;
