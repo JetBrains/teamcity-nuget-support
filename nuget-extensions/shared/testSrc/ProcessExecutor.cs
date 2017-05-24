86e644f4add621893d69a069fa72486ff6c499c1
Dmitry.Tretyakov <dtretyakov@gmail.com>
2017-05-24 18:07:37 +0300

    Fix dead lock while reading test output

--- nuget-extensions/shared/testSrc/ProcessExecutor.cs
+++ nuget-extensions/shared/testSrc/ProcessExecutor.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Text;
-using System.Threading;
 using NUnit.Framework;
 using System.Linq;
 
@@ -25,30 +23,26 @@ namespace JetBrains.TeamCity.NuGet.Tests
 
       Console.Out.WriteLine("Starting: " + pi.FileName + " " + pi.Arguments);
 
-      var process = Process.Start(pi);
-      process.StandardInput.Close();
-      Func<StreamReader, string> readOutput = stream =>
-                                                {
-                                                  var result = "";
-                                                  var th = new Thread(delegate()
-                                                                        {
-                                                                          var sb = new StringBuilder();
-                                                                          int i;
-                                                                          while ((i = stream.Read()) >= 0)
-                                                                            sb.Append((char) i);
-                                                                          result = sb.ToString();
-                                                                        })
-                                                             {Name = "Process output reader " + process.Id};
-                                                  th.Start();
-                                                  th.Join(TimeSpan.FromMinutes(10));
-                                                  return result;
-                                                };
+      var process = new Process {StartInfo = pi};
+      
+      var errorDataBuilder = new StringBuilder();
+      process.ErrorDataReceived += delegate(object sender, DataReceivedEventArgs e)
+                                   {
+                                     errorDataBuilder.Append(e.Data);
+                                   };
+      
+      var outputDataBuilder = new StringBuilder();
+      process.OutputDataReceived += delegate(object sender, DataReceivedEventArgs e)
+                                    {
+                                      outputDataBuilder.Append(e.Data);
+                                    };
+      
+      process.Start();
+      process.BeginOutputReadLine();
+      process.BeginErrorReadLine();
+      process.WaitForExit(30000);
 
-      string output = readOutput(process.StandardOutput);
-      string error = readOutput(process.StandardError);
-      process.WaitForExit();
-
-      return new Result(output, error, process.ExitCode);
+      return new Result(outputDataBuilder.ToString(), errorDataBuilder.ToString(), process.ExitCode);
     }
 
     public class Result
