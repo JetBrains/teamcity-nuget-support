62decd4573fb93c02e11226b666a2d0d2dd2f017
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-07-04 15:33:50 +0200

    Report last access date for packages while cleaning NuGet cache
    Issue: TW-50553

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/NuGetCacheCleaner.java
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/NuGetCacheCleaner.java
@@ -15,7 +15,7 @@ import java.util.Date;
  */
 public class NuGetCacheCleaner implements DirectoryCleanersProvider {
 
-  private static final String CLEANER_FORMAT = "Registering directory %s for cleaning";
+  private static final String CLEANER_FORMAT = "Registering packages in %s for cleaning";
   private static final Logger LOG = Logger.getInstance(NuGetCacheCleaner.class.getName());
 
   @NotNull
@@ -29,21 +29,22 @@ public class NuGetCacheCleaner implements DirectoryCleanersProvider {
                                         @NotNull final DirectoryCleanersRegistry registry) {
     final String nugetPackages = System.getenv("NUGET_PACKAGES");
     if (!StringUtil.isEmptyOrSpaces(nugetPackages)) {
+      // Overriden packages cache since NuGet 3.0
       final File globalPackages = new File(nugetPackages);
       if (globalPackages.isAbsolute() && globalPackages.exists()) {
-        LOG.info(String.format(CLEANER_FORMAT, globalPackages));
-        registry.addCleaner(globalPackages, new Date());
+        registerV3Cache(registry, globalPackages);
       }
     }
 
     final String localAppData = System.getenv("LOCALAPPDATA");
     if (!StringUtil.isEmptyOrSpaces(localAppData)) {
+      // Packages cache up to NuGet 3.0
       final File nugetCache = new File(localAppData, "NuGet/Cache");
       if (nugetCache.exists()) {
-        LOG.info(String.format(CLEANER_FORMAT, nugetCache));
-        registry.addCleaner(nugetCache, new Date());
+        registerCache(registry, nugetCache);
       }
 
+      // HTTP response cache
       final File nugetV3Cache = new File(localAppData, "NuGet/v3-cache");
       if (nugetV3Cache.exists()) {
         LOG.info(String.format(CLEANER_FORMAT, nugetV3Cache));
@@ -53,10 +54,49 @@ public class NuGetCacheCleaner implements DirectoryCleanersProvider {
 
     final String userHome = System.getProperty("user.home");
     if (!StringUtil.isEmptyOrSpaces(userHome)) {
+      // Packages cache since NuGet 3.0
       final File homeCache = new File(userHome, ".nuget/packages");
       if (homeCache.exists()) {
-        LOG.info(String.format(CLEANER_FORMAT, homeCache));
-        registry.addCleaner(homeCache, new Date());
+        registerV3Cache(registry, homeCache);
+      }
+    }
+  }
+
+  // Directory structure is as follows:
+  // %package-name%.%package-version%.nupkg
+  private static void registerCache(DirectoryCleanersRegistry registry, File directory) {
+    LOG.info(String.format(CLEANER_FORMAT, directory));
+
+    final File[] packages = directory.listFiles();
+    if (packages != null) {
+      for (File file : packages) {
+        if (file.equals(directory)) continue;
+        if (file.isDirectory()) continue;
+
+        registry.addCleaner(file, new Date(file.lastModified()));
+      }
+    }
+  }
+
+  // Directory structure is as follows:
+  // %package-name%/%version%/*.*
+  private static void registerV3Cache(DirectoryCleanersRegistry registry, File directory) {
+    LOG.info(String.format(CLEANER_FORMAT, directory));
+
+    final File[] packages = directory.listFiles();
+    if (packages != null) {
+      for (File file : packages) {
+        if (file.equals(directory)) continue;
+        if (!file.isDirectory()) continue;
+
+        final File[] versions = file.listFiles();
+        if (versions != null) {
+          for (File version : versions) {
+            registry.addCleaner(version, new Date(version.lastModified()));
+          }
+        }
+
+        registry.addCleaner(file, new Date());
       }
     }
   }
