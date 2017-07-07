b986f3743baefef137f331b7e4c6b674a18e677a
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-07-07 13:26:57 +0200

    NuGet Trigger doesn't work with private feeds
    Issue: TW-49914

--- nuget-server/src/jetbrains/buildServer/nuget/server/trigger/TriggerConstants.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/trigger/TriggerConstants.java
@@ -16,6 +16,8 @@
 
 package jetbrains.buildServer.nuget.server.trigger;
 
+import jetbrains.buildServer.agent.Constants;
+
 /**
  * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
  * Date: 12.07.11 15:00
@@ -23,11 +25,11 @@ package jetbrains.buildServer.nuget.server.trigger;
 public interface TriggerConstants {
   String TRIGGER_ID = "nuget.simple";
   String NUGET_PATH_PARAM_NAME = "nuget.exe";
-  
+
   String SOURCE = "nuget.source";
   String PACKAGE = "nuget.package";
   String VERSION = "nuget.version";
   String INCLUDE_PRERELEASE = "nuget.include.prerelease";
   String USERNAME = "nuget.username";
-  String PASSWORD = "nuget.password";
+  String PASSWORD = Constants.SECURE_PROPERTY_PREFIX + "nuget.password";
 }
