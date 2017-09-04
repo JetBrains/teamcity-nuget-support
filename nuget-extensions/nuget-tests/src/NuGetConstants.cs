0ce7b4b4173c4cdd4e38ccd1fc57192f31917769
Dmitry Tretyakov <dtretyakov@gmail.com>
2017-09-04 16:59:10 +0200

    Use direct nuget public feed URLs

--- nuget-extensions/nuget-tests/src/NuGetConstants.cs
+++ nuget-extensions/nuget-tests/src/NuGetConstants.cs
@@ -2,8 +2,8 @@ namespace JetBrains.TeamCity.NuGet.Tests
 {
   public static class NuGetConstants
   {
-    public const string DefaultFeedUrl_v1 = "https://go.microsoft.com/fwlink/?LinkID=206669";
-    public const string DefaultFeedUrl_v2 = "https://go.microsoft.com/fwlink/?LinkID=230477";
+    public const string DefaultFeedUrl_v1 = "https://packages.nuget.org/v1/FeedService.svc/";
+    public const string DefaultFeedUrl_v2 = "https://www.nuget.org/api/v2/";
     public const string NuGetDevFeed = "https://dotnet.myget.org/F/nuget-build/api/v2/";
   }
 }
