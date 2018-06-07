f7aded623d9bc72a14e20d69633e03e166b2fd34
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-06-07 10:46:05 +0200

    Show NuGet feed URLs with guestAuth if available
    Issue: TW-55245

--- nuget-feed/src/jetbrains/buildServer/nuget/feed/server/NuGetFeedUrlsProvider.kt
+++ nuget-feed/src/jetbrains/buildServer/nuget/feed/server/NuGetFeedUrlsProvider.kt
@@ -5,6 +5,7 @@ import jetbrains.buildServer.nuget.feed.server.packages.NuGetRepository
 import jetbrains.buildServer.serverSide.DataItem
 import jetbrains.buildServer.serverSide.ProjectDataFetcher
 import jetbrains.buildServer.serverSide.ProjectManager
+import jetbrains.buildServer.serverSide.auth.LoginConfiguration
 import jetbrains.buildServer.serverSide.packages.impl.RepositoryManager
 import jetbrains.buildServer.util.browser.Browser
 import jetbrains.buildServer.web.util.WebUtil
@@ -12,7 +13,8 @@ import javax.ws.rs.core.UriBuilder
 
 class NuGetFeedUrlsProvider(private val myProjectManager: ProjectManager,
                             private val myRepositoryManager: RepositoryManager,
-                            private val myRootUrlHolder: RootUrlHolder) : ProjectDataFetcher {
+                            private val myRootUrlHolder: RootUrlHolder,
+                            private val myLoginConfiguration: LoginConfiguration) : ProjectDataFetcher {
 
     override fun getType() = "NuGetFeedUrls"
 
@@ -22,10 +24,15 @@ class NuGetFeedUrlsProvider(private val myProjectManager: ProjectManager,
         return myRepositoryManager.getRepositories(buildTypeByExternalId.project, true)
                 .filterIsInstance<NuGetRepository>()
                 .flatMap {
-                    it.urlPaths.map {
-                        val contextPath = WebUtil.combineContextPath(WebUtil.HTTP_AUTH_PREFIX, it)
-                        DataItem(uriBuilder.replacePath(contextPath).build().toString(), null)
+                    it.urlPaths.flatMap {
+                        arrayListOf<String>(WebUtil.combineContextPath(WebUtil.HTTP_AUTH_PREFIX, it)).apply {
+                            if (myLoginConfiguration.isGuestLoginAllowed) {
+                                this.add(WebUtil.combineContextPath(WebUtil.GUEST_AUTH_PREFIX, it))
+                            }
+                        }
                     }
+                }.map {
+                    DataItem(uriBuilder.replacePath(it).build().toString(), null)
                 }.toMutableList()
     }
 }
