b322f9733a3f48313448452c90a03393747f7af7
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-09-11 16:12:04 +0200

    Don't set credentials plugin for dotnet in docker wrapper
    Issue: TW-52337

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/credentials/DotNetCredentialsPathProvider.kt
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/credentials/DotNetCredentialsPathProvider.kt
@@ -10,8 +10,10 @@ class DotNetCredentialsPathProvider(private val provider: NuGetTeamCityProvider)
         get() = listOf("dotnet.cli")
 
     override fun getPluginPath(runner: BuildRunnerContext): String? {
+        // We need somehow detect dotnet version in container
+        // since in images with .NET CLI < 2.1.400 it fails with various problems
         if (runner.isVirtualContext) {
-            provider.pluginCorePath
+            return null
         }
 
         val version = runner.configParameters["DotNetCLI"]?.let { SemanticVersion.valueOf(it) } ?: return null
