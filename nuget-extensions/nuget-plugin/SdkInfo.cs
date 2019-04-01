8ccba68c25d9f5db260c10b7fdade48939997187
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2019-04-01 16:41:52 +0300

    TW-59793 TeamCity NuGet credential provider causing FileNotFoundException when executing dotnet publish step - add sdk detection in the provider

---
+++ nuget-extensions/nuget-plugin/SdkInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using NuGet.Versioning;
+
+namespace JetBrains.TeamCity.NuGet
+{
+  internal class SdkInfo
+  {
+    public bool TryGetSdkVersion(out SemanticVersion version)
+    {
+      try
+      {
+        var sdkPath = Environment.GetEnvironmentVariable("MSBuildSDKsPath");
+        if (sdkPath != null)
+        {
+          var versionFile = Path.Combine(sdkPath, "..", ".version");
+          if (File.Exists(versionFile))
+          {
+            var lines = File.ReadAllLines(versionFile);
+            if (lines.Length > 1)
+            {
+              version = SemanticVersion.Parse(lines[1]);
+              return true;
+            }
+          }
+        }
+      }
+      catch
+      {
+        // ignored
+      }
+
+      version = default(SemanticVersion);
+      return false;
+    }
+  }
+}
