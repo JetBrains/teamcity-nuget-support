74fd4910049241d2e0be6d4eb3d8a5a181f50062
Pavel Sher <pavel.sher@jetbrains.com>
2019-05-15 18:11:28 +0200

    add proper default parameter for NuGet update mode

--- nuget-server/src/jetbrains/buildServer/nuget/server/runner/install/PackagesInstallerRunnerDefaults.java
+++ nuget-server/src/jetbrains/buildServer/nuget/server/runner/install/PackagesInstallerRunnerDefaults.java
@@ -16,6 +16,9 @@
 
 package jetbrains.buildServer.nuget.server.runner.install;
 
+import jetbrains.buildServer.nuget.common.PackagesConstants;
+import jetbrains.buildServer.nuget.common.PackagesUpdateMode;
+
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -29,6 +32,7 @@ public class PackagesInstallerRunnerDefaults {
   public Map<String,String> getRunnerProperties(){
     final TreeMap<String, String> map = new TreeMap<String, String>();
     map.put(NUGET_USE_RESTORE_COMMAND, NUGET_USE_RESTORE_COMMAND_RESTORE_MODE);
+    map.put(PackagesConstants.NUGET_UPDATE_MODE, PackagesUpdateMode.FOR_SLN.getName());
     return map;
   }
 }
