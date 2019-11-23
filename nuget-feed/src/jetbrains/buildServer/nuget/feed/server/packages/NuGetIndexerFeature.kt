10593a285f6c286cced91ca60f1d290abfdf423f
pavel.sher <pavel.sher@gmail.com>
2019-11-23 19:45:47 +0100

    fix TW-63264 & TW-63284

--- nuget-feed/src/jetbrains/buildServer/nuget/feed/server/packages/NuGetIndexerFeature.kt
+++ nuget-feed/src/jetbrains/buildServer/nuget/feed/server/packages/NuGetIndexerFeature.kt
@@ -2,14 +2,11 @@ package jetbrains.buildServer.nuget.feed.server.packages
 
 import jetbrains.buildServer.controllers.BaseController
 import jetbrains.buildServer.controllers.admin.projects.BuildTypeForm
-import jetbrains.buildServer.nuget.common.index.PackageConstants
 import jetbrains.buildServer.nuget.feed.server.NuGetFeedConstants
 import jetbrains.buildServer.nuget.feed.server.NuGetUtils
 import jetbrains.buildServer.nuget.feed.server.index.NuGetFeedData
 import jetbrains.buildServer.serverSide.*
-import jetbrains.buildServer.serverSide.packages.RepositoryConstants
 import jetbrains.buildServer.serverSide.packages.impl.RepositoryManager
-import jetbrains.buildServer.util.StringUtil
 import jetbrains.buildServer.web.openapi.PluginDescriptor
 import jetbrains.buildServer.web.openapi.WebControllerManager
 import org.springframework.web.servlet.ModelAndView
@@ -18,7 +15,6 @@ import javax.servlet.http.HttpServletResponse
 
 class NuGetIndexerFeature(pluginDescriptor: PluginDescriptor,
                           web: WebControllerManager,
-                          eventDispatcher: ProjectsModelEventDispatcher,
                           private val myProjectManager: ProjectManager,
                           private val myRepositoryManager: RepositoryManager) : BuildFeature() {
 
@@ -43,64 +39,6 @@ class NuGetIndexerFeature(pluginDescriptor: PluginDescriptor,
                     }
 
                 return modelAndView
-            }
-        })
-
-        eventDispatcher.addListener(object : ProjectsModelListenerAdapter() {
-            override fun projectFeatureChanged(project: SProject, before: SProjectFeatureDescriptor, after: SProjectFeatureDescriptor) {
-                if (before.type != RepositoryConstants.PACKAGES_FEATURE_TYPE) {
-                    return
-                }
-                if (before.parameters[RepositoryConstants.REPOSITORY_TYPE_KEY] != PackageConstants.NUGET_PROVIDER_ID) {
-                    return
-                }
-
-                val newName = after.parameters[RepositoryConstants.REPOSITORY_NAME_KEY]
-                val oldName = before.parameters[RepositoryConstants.REPOSITORY_NAME_KEY]
-                if (oldName == newName || oldName == null || newName == null) {
-                    return
-                }
-
-                val oldReference = Pair(project.externalId, oldName)
-                val newReference = Pair(project.externalId, newName)
-
-                updateFeedReferences(project, oldReference, newReference)
-                project.projects.forEach {
-                    updateFeedReferences(it, oldReference, newReference)
-                }
-            }
-
-            override fun projectFeatureRemoved(project: SProject, feature: SProjectFeatureDescriptor) {
-                if (feature.type != RepositoryConstants.PACKAGES_FEATURE_TYPE) {
-                    return
-                }
-                if (feature.parameters[RepositoryConstants.REPOSITORY_TYPE_KEY] != PackageConstants.NUGET_PROVIDER_ID) {
-                    return
-                }
-
-                val name = feature.parameters[RepositoryConstants.REPOSITORY_NAME_KEY] ?: return
-                val reference = Pair(project.externalId, name)
-
-                deleteFeedReferences(project, reference)
-                project.projects.forEach {
-                    deleteFeedReferences(it, reference)
-                }
-            }
-
-            override fun projectRemoved(project: SProject) {
-                project.getAvailableFeaturesOfType(RepositoryConstants.PACKAGES_FEATURE_TYPE).forEach {feature ->
-                    if (feature.parameters[RepositoryConstants.REPOSITORY_TYPE_KEY] != PackageConstants.NUGET_PROVIDER_ID) {
-                        return
-                    }
-
-                    val name = feature.parameters[RepositoryConstants.REPOSITORY_NAME_KEY] ?: return
-                    val reference = Pair(project.externalId, name)
-
-                    deleteFeedReferences(project, reference)
-                    project.projects.forEach {
-                        deleteFeedReferences(it, reference)
-                    }
-                }
             }
         })
 
@@ -145,39 +83,6 @@ class NuGetIndexerFeature(pluginDescriptor: PluginDescriptor,
                 }
             }
             return@PropertiesProcessor invalidProperties
-        }
-    }
-
-    private fun updateFeedReferences(project: SProject, oldReference: Pair<String, String>, newReference: Pair<String, String>) {
-        processFeedReferences(project,  oldReference, { buildType, feature ->
-            val parameters = feature.parameters.toMutableMap()
-            parameters[NuGetFeedConstants.NUGET_INDEXER_FEED] = newReference.toString()
-            buildType.updateBuildFeature(feature.id, feature.type, parameters)
-        })
-    }
-
-    private fun deleteFeedReferences(project: SProject, oldReference: Pair<String, String>) {
-        processFeedReferences(project,  oldReference, { buildType, feature ->
-            buildType.removeBuildFeature(feature.id)
-        })
-    }
-
-    private fun processFeedReferences(project: SProject, reference: Pair<String, String>, action: (SBuildType, SBuildFeatureDescriptor) -> Unit) {
-        project.buildTypes.forEach {buildType ->
-            var updated = false
-            buildType.getBuildFeaturesOfType(NuGetFeedConstants.NUGET_INDEXER_TYPE).forEach {feature ->
-                feature.parameters[NuGetFeedConstants.NUGET_INDEXER_FEED]?.let {
-                    NuGetUtils.feedIdToData(it)?.let {
-                        if (it == reference) {
-                            action(buildType, feature)
-                            updated = true
-                        }
-                    }
-                }
-            }
-            if (updated) {
-                buildType.persist()
-            }
         }
     }
 }
--- nuget-feed/src/jetbrains/buildServer/serverSide/packages/impl/RepositoryManagerImpl.java
+++ nuget-feed/src/jetbrains/buildServer/serverSide/packages/impl/RepositoryManagerImpl.java
@@ -1,9 +1,9 @@
 package jetbrains.buildServer.serverSide.packages.impl;
 
+import com.intellij.openapi.util.Pair;
+import jetbrains.buildServer.nuget.feed.server.NuGetFeedConstants;
 import jetbrains.buildServer.nuget.feed.server.index.NuGetFeedData;
-import jetbrains.buildServer.serverSide.ProjectManager;
-import jetbrains.buildServer.serverSide.SProject;
-import jetbrains.buildServer.serverSide.SProjectFeatureDescriptor;
+import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.serverSide.impl.ProjectFeatureDescriptorFactory;
 import jetbrains.buildServer.serverSide.impl.ProjectFeatureDescriptorImpl;
 import jetbrains.buildServer.serverSide.metadata.BuildMetadataEntry;
@@ -18,22 +18,27 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RepositoryManagerImpl implements RepositoryManager, CachingTypedIdGenerator {
 
     private final RepositoryRegistry myRepositoryRegistry;
     private final ProjectManager myProjectManager;
     private final MetadataStorage myMetadataStorage;
+  private final ConfigActionFactory myConfigActionFactory;
 
-    public RepositoryManagerImpl(@NotNull final RepositoryRegistry repositoryRegistry,
+  public RepositoryManagerImpl(@NotNull final RepositoryRegistry repositoryRegistry,
                                  @NotNull final ProjectManager projectManager,
                                  @NotNull final MetadataStorage metadataStorage,
-                                 @NotNull final ProjectFeatureDescriptorFactory descriptorFactory) {
+                                 @NotNull final ProjectFeatureDescriptorFactory descriptorFactory,
+                                 @NotNull final ConfigActionFactory configActionFactory) {
         myRepositoryRegistry = repositoryRegistry;
         myProjectManager = projectManager;
         myMetadataStorage = metadataStorage;
+        myConfigActionFactory = configActionFactory;
         descriptorFactory.registerGenerator(RepositoryConstants.PACKAGES_FEATURE_TYPE, this);
     }
 
@@ -45,7 +50,7 @@ public class RepositoryManagerImpl implements RepositoryManager, CachingTypedIdG
                 repository.getParameters(),
                 project.getProjectId()
         ));
-        project.persist();
+        project.persist(myConfigActionFactory.createAction(project, "NuGet feed with name \"" + repository.getName() + "\" was added"));
     }
 
     @Nullable
@@ -63,7 +68,7 @@ public class RepositoryManagerImpl implements RepositoryManager, CachingTypedIdG
             throw new IllegalArgumentException(String.format("Package repository %s not found", name));
         }
         project.removeFeature(feature.getId());
-        project.persist();
+        project.persist(myConfigActionFactory.createAction(project, "NuGet feed with name \"" + name + "\" was removed"));
 
         NuGetFeedData feedData = new NuGetFeedData(project.getProjectId(), name);
         Iterator<BuildMetadataEntry> entries = myMetadataStorage.getAllEntries(feedData.getKey());
@@ -94,18 +99,21 @@ public class RepositoryManagerImpl implements RepositoryManager, CachingTypedIdG
     @Override
     public void updateRepository(@NotNull SProject project, @NotNull String oldName, @NotNull Repository repository) {
         final String type = repository.getType().getType();
-        final SProjectFeatureDescriptor feature = project.findFeatureById(getProjectFeatureName(type, oldName));
+        String oldId = getProjectFeatureName(type, oldName);
+        final SProjectFeatureDescriptor feature = project.findFeatureById(oldId);
         if (feature == null) {
             throw new IllegalArgumentException(String.format("Package repository %s not found", oldName));
         }
 
         if (oldName.equals(repository.getName())) {
             project.updateFeature(feature.getId(), feature.getType(), repository.getParameters());
-            project.persist();
+            project.persist(myConfigActionFactory.createAction(project, "NuGet feed settings changed"));
         } else {
             addRepository(project, repository);
             project.removeFeature(feature.getId());
-            project.persist();
+            project.persist(myConfigActionFactory.createAction(project, "NuGet feed settings changed"));
+
+            updateFeedReferences(project, new Pair<>(project.getExternalId(), oldName), new Pair<>(project.getExternalId(), repository.getName()));
 
             NuGetFeedData feedData = new NuGetFeedData(project.getProjectId(), oldName);
             Iterator<BuildMetadataEntry> entries = myMetadataStorage.getAllEntries(feedData.getKey());
@@ -117,7 +125,40 @@ public class RepositoryManagerImpl implements RepositoryManager, CachingTypedIdG
         }
     }
 
-    @Nullable
+    private void updateFeedReferences(@NotNull SProject project, Pair<String, String> oldReference, Pair<String, String> newReference) {
+        project.getOwnBuildTypes().forEach(buildType -> {
+            if (updateBuildFeatures(oldReference, newReference, buildType)) {
+                buildType.persist(myConfigActionFactory.createAction(buildType.getProject(), "NuGet feed reference changed"));
+            }
+        });
+
+        project.getOwnBuildTypeTemplates().forEach(tpl -> {
+            if (updateBuildFeatures(oldReference, newReference, tpl)) {
+                tpl.persist(myConfigActionFactory.createAction(tpl.getProject(), "NuGet feed reference changed"));
+            }
+        });
+
+        for (SProject subProject: project.getOwnProjects()) updateFeedReferences(subProject, oldReference, newReference);
+    }
+
+  private boolean updateBuildFeatures(@NotNull Pair<String, String> oldReference, @NotNull Pair<String, String> newReference, @NotNull BuildTypeSettings buildType) {
+    AtomicBoolean updated = new AtomicBoolean(false);
+    buildType.getBuildFeaturesOfType(NuGetFeedConstants.NUGET_INDEXER_TYPE).forEach(feature -> {
+        String feedName = feature.getParameters().get(NuGetFeedConstants.NUGET_INDEXER_FEED);
+        if (feedName == null) return;
+
+        if (feedName.equals(oldReference.first + "/" + oldReference.second)) {
+            Map<String, String> newParams = new HashMap<>(feature.getParameters());
+            newParams.put(NuGetFeedConstants.NUGET_INDEXER_FEED, newReference.first + "/" + newReference.second);
+            buildType.updateBuildFeature(feature.getId(), feature.getType(), newParams);
+            updated.set(true);
+        }
+    });
+
+    return updated.get();
+  }
+
+  @Nullable
     private Repository getRepository(SProjectFeatureDescriptor feature) {
         final Map<String, String> parameters = feature.getParameters();
         final String type = parameters.get(RepositoryConstants.REPOSITORY_TYPE_KEY);
