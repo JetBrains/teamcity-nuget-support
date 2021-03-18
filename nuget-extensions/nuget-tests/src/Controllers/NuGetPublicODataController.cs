47e58871a5220e8079a6f5948e3b62df5292f8a1
Ilya Fomenko <ilya.fomenko@jetbrains.com>
2021-03-18 21:31:51 +0300

    Fixed failed NuGet tests due to deprecation of some V2 API on nuget.org.

---
+++ nuget-extensions/nuget-tests/src/Controllers/NuGetPublicODataController.cs
@@ -0,0 +1,12 @@
+﻿using NuGet.Server.V2.Controllers;
+
+namespace JetBrains.TeamCity.NuGet.Tests.Controllers
+{
+  public class NuGetPublicODataController : NuGetODataController
+  {
+    public NuGetPublicODataController()
+      : base(ServerPackageRepository.Instance)
+    {
+    }
+  }
+}
