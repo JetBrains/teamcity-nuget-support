98dd14b90e063f23488523148885db972442b073
Andre Rocha <andre.rocha@jetbrains.com>
2022-09-13 11:49:57 +0100

    Revert "TW-77411 Switch bundled plugins to use a persistent cache of settings"
    This reverts commit 0b4ddead41191afcd7d19eb75f69e12f99ebfbe6.

--- nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/credentials/CredentialProviderPropertiesExtension.kt
+++ nuget-agent/src/jetbrains/buildServer/nuget/agent/runner/credentials/CredentialProviderPropertiesExtension.kt
@@ -1,34 +1,37 @@
 package jetbrains.buildServer.nuget.agent.runner.credentials
 
-import jetbrains.buildServer.ExtensionHolder
-import jetbrains.buildServer.agent.config.AgentConfigurationAdapter
-import jetbrains.buildServer.agent.config.AgentConfigurationSnapshot
+import jetbrains.buildServer.agent.AgentLifeCycleAdapter
+import jetbrains.buildServer.agent.AgentLifeCycleListener
+import jetbrains.buildServer.agent.BuildAgent
+import jetbrains.buildServer.agent.ToolProvider
+import jetbrains.buildServer.nuget.common.PackagesConstants
 import jetbrains.buildServer.nuget.common.exec.NuGetTeamCityProvider
+import jetbrains.buildServer.util.EventDispatcher
 import org.apache.log4j.Logger
 import java.io.File
 
 class CredentialProviderPropertiesExtension(
-        extensionHolder: ExtensionHolder,
+        events: EventDispatcher<AgentLifeCycleListener>,
         private val _nuGetTeamCityProvider: NuGetTeamCityProvider)
-    : AgentConfigurationAdapter() {
+    : AgentLifeCycleAdapter() {
 
     init {
-        extensionHolder.registerExtension(AgentConfigurationSnapshot::class.java, javaClass.name, this)
+        events.addListener(this)
     }
 
-    override fun addParameters(parameters: MutableMap<String, String>) {
+    override fun beforeAgentConfigurationLoaded(agent: BuildAgent) {
         LOG.debug("Locating credential providers")
-        addParam(parameters,"DotNetCredentialProvider4.0.0_Path", _nuGetTeamCityProvider.pluginFxPath)
+        addParam(agent,"DotNetCredentialProvider4.0.0_Path", _nuGetTeamCityProvider.pluginFxPath)
         for (version in (1 .. 10).filter { it != 4 }) {
-            addParam(parameters,"DotNetCredentialProvider$version.0.0_Path", _nuGetTeamCityProvider.getPluginCorePath(version))
+            addParam(agent,"DotNetCredentialProvider$version.0.0_Path", _nuGetTeamCityProvider.getPluginCorePath(version))
         }
     }
 
-    private fun addParam(parameters: MutableMap<String, String>, paramName: String, credentialProviderPath: String) {
+    private fun addParam(agent: BuildAgent, paramName: String, credentialProviderPath: String) {
         val credentialProvider = File(credentialProviderPath)
         if (credentialProvider.exists() && credentialProvider.isFile) {
             LOG.info("Found .NET credential provider $paramName at $credentialProviderPath")
-            parameters.put(paramName, credentialProviderPath)
+            agent.configuration.addConfigurationParameter(paramName, credentialProviderPath)
         }
     }
 
