43e9f26dadac6537e1fcba2b8dffb6029a4b4e1f
Andrey Koltsov <a99920641@gmail.com>
2026-09-09 13:25:52 +0200

    Conditionally return client origin in case only global URL is configured for the server

--- nuget-feed/src/jetbrains/buildServer/nuget/feed/server/impl/NuGetFeedRootUrlResolver.kt
+++ nuget-feed/src/jetbrains/buildServer/nuget/feed/server/impl/NuGetFeedRootUrlResolver.kt
@@ -5,12 +5,28 @@ import javax.servlet.http.HttpServletRequest
 
 class NuGetFeedRootUrlResolver(private val myRootUrlResolver: ProjectAwareRootUrlResolver) {
 
+    /**
+     * The URL a NuGet client should use for a feed: the URL configured for the feed's project, or the URL the client is already talking to
+     * when the project redefines none.
+     */
     fun getRootUrl(request: HttpServletRequest, projectExternalId: String?): String {
-        val projectRootUrl = myRootUrlResolver.getRootUrlByProjectExternalId(projectExternalId)
-        val rootUrl = if (projectRootUrl == myRootUrlResolver.rootUrl) HttpServletRequestUtil.getRootUrl(request) else projectRootUrl
-        return rootUrl.removeSuffix("/")
+        val configuredRootUrl = getRootUrl(projectExternalId)
+        return if (configuredRootUrl != globalServerUrl) {
+            // Project redefines the URL
+            configuredRootUrl
+        } else {
+            // Keep the client on the origin it actually reached as the global URL might still be default
+            withoutTrailingSlash(HttpServletRequestUtil.getRootUrl(request))
+        }
     }
 
     fun getRootUrlWithAuthenticationType(request: HttpServletRequest, projectExternalId: String?): String =
         getRootUrl(request, projectExternalId) + HttpServletRequestUtil.getAuthenticationTypePath(request)
+
+    fun getRootUrl(projectExternalId: String?): String =
+        withoutTrailingSlash(myRootUrlResolver.getRootUrlByProjectExternalId(projectExternalId))
+
+    private val globalServerUrl: String get() = withoutTrailingSlash(myRootUrlResolver.rootUrl)
+
+    private fun withoutTrailingSlash(rootUrl: String) = rootUrl.removeSuffix("/")
 }
